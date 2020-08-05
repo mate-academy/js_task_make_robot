bb1907798351444003ff887309cb39bcef86a0ea
Alisa <aliceblunt8@gmail.com>
2020-08-05 15:26:27 +0300

    small fix

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -17,7 +17,7 @@
  *  - Movement methods must be able to be used with a chain.
  *    robot.goForward().goForward().goForward().goLeft()
  *  - Default methods that move the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
+ *    This step can be increased by passing the desired number to the method.
  *    Negative numbers should not affect the location of the robot. goLeft(3)
  *  - The coordinates of the robot must be stored in the object coords,
  *    the keys x and y inside the robot.
@@ -48,8 +48,8 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      // eslint-disable-next-line max-len
-      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+      return `name: ${this.name}, chip version: ${this.version},`
+      + ` wheels: ${this.wheels}`;
     },
 
     get location() {
@@ -61,33 +61,33 @@ function makeRobot(name, wheels, version) {
       this.coords.y = 500;
     },
 
-    goForward: function(value = 1) {
-      if (value > 0) {
-        this.coords.y += value;
+    goForward: function(step = 1) {
+      if (step > 0) {
+        this.coords.y += step;
       }
 
       return this;
     },
 
-    goBack: function(value = 1) {
-      if (value > 0) {
-        this.coords.y -= value;
+    goBack: function(step = 1) {
+      if (step > 0) {
+        this.coords.y -= step;
       }
 
       return this;
     },
 
-    goRight: function(value = 1) {
-      if (value > 0) {
-        this.coords.x += value;
+    goRight: function(step = 1) {
+      if (step > 0) {
+        this.coords.x += step;
       }
 
       return this;
     },
 
-    goLeft: function(value = 1) {
-      if (value > 0) {
-        this.coords.x -= value;
+    goLeft: function(step = 1) {
+      if (step > 0) {
+        this.coords.x -= step;
       }
 
       return this;
