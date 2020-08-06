a866d8a11fb96d9e05f574f3426b3611a5cad6db
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-06 17:32:19 +0300

    fixed solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -57,7 +57,7 @@ function makeRobot(name, wheels, version) {
     },
 
     goBack(value = 1) {
-      if (value >= 0) {
+      if (value > 0) {
         this.coords.y -= value;
       }
 
@@ -65,7 +65,7 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(value = 1) {
-      if (value >= 0) {
+      if (value > 0) {
         this.coords.y += value;
       }
 
@@ -73,7 +73,7 @@ function makeRobot(name, wheels, version) {
     },
 
     goLeft(value = 1) {
-      if (value >= 0) {
+      if (value > 0) {
         this.coords.x -= value;
       }
 
@@ -81,7 +81,7 @@ function makeRobot(name, wheels, version) {
     },
 
     goRight(value = 1) {
-      if (value >= 0) {
+      if (value > 0) {
         this.coords.x += value;
       }
 
