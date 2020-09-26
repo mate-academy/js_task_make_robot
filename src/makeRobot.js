bd2fff9ca63c7f0df2e53bced6f81c1d271df1e7
KirillLutsenko <mnrovski68@gmail.com>
2020-09-26 19:45:10 +0300

    Make Robot Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -1,29 +1,28 @@
 'use strict';
 
 /**
- * Mate Robot Factory impressed by your success, they are ready to accept
- * you into the Tech team, you will learn to program robots together
- * with the team! Are you in business As a test task, you will need to
- * program our equipment that makes robots.
- *
- * Create a makeRobot function that takes the string name and the number
- * wheels, version and returns the robot object.
- * The robot coming off the assembly line must be able to:
- *  - Provide information about yourself through getter info.
- *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
- *  - Provide the coordinates of your location via getter location.
- *    robot.location === '%name%: x=14, y=21'
- *  - Have methods to move goForward, goBack, goRight, goLeft.
- *  - Movement methods must be able to be used with a chain.
- *    robot.goForward().goForward().goForward().goLeft()
- *  - Default methods that move the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordinates of the robot must be stored in the object coords,
- *    the keys x and y inside the robot.
- *  - The robot must be able to request the evacuation of robot.evacuate(),
- *    which will call rescuers and transfer it to the service center
- *    at the coordinates x: 1400, y: 500.
+Mate Robot Factory impressed by your success, they are ready to accept
+you into the Tech team, you will learn to program robots together
+with the team! Are you in business As a test task, you will need to
+program our equipment that makes robots.
+Create a makeRobot function that takes the string name and the number
+wheels, version and returns the robot object.
+The robot coming off the assembly line must be able to:
+  - Provide information about yourself through getter info.
+    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
+  - Provide the coordinates of your location via getter location.
+    robot.location === '%name%: x=14, y=21'
+  - Have methods to move goForward, goBack, goRight, goLeft.
+  - Movement methods must be able to be used with a chain.
+    robot.goForward().goForward().goForward().goLeft()
+  - Default methods that move the work by 1 in the right direction.
+    This value can be increased by passing the desired number to the method.
+    Negative numbers should not affect the location of the robot. goLeft(3)
+  - The coordinates of the robot must be stored in the object coords,
+    the keys x and y inside the robot.
+  - The robot must be able to request the evacuation of robot.evacuate(),
+    which will call rescuers and transfer it to the service center
+    at the coordinates x: 1400, y: 500.
  *
  * @typedef {object} Robot
  * @property {string} name
@@ -38,7 +37,63 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    version,
+    wheels,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      // eslint-disable-next-line max-len
+      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(step = 1) {
+      if (step > 0) {
+        this.coords.y += step;
+      }
+
+      return this;
+    },
+
+    goBack(step = 1) {
+      if (step > 0) {
+        this.coords.y -= step;
+      }
+
+      return this;
+    },
+
+    goRight(step = 1) {
+      if (step > 0) {
+        this.coords.x += step;
+      }
+
+      return this;
+    },
+
+    goLeft(step = 1) {
+      if (step > 0) {
+        this.coords.x -= step;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.y = 500;
+      this.coords.x = 1400;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
