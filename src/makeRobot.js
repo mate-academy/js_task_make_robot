3a96f2a587ae4bf0fe79e8e0dafb0a584d2ae49d
Evgenii <voznukzhenia@gmail.com>
2020-09-28 13:04:13 +0300

    fixed back quotes

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -49,8 +49,7 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      return 'name: ' + this.name + ', chip version: ' + this.version
-      + ', wheels: ' + this.wheels;
+      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
     },
 
     goForward(step = 1) {
@@ -86,7 +85,7 @@ function makeRobot(name, wheels, version) {
     },
 
     get location() {
-      return this.name + ': x=' + this.coords.x + ', y=' + this.coords.y;
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
 
     evacuate() {
