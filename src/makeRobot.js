a8d8f7927f3116c45557ed38b3ed92e72b190592
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-12 01:19:49 +0200

    code was optimized

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -1,31 +1,31 @@
 'use strict';
 
 /**
- * Mate Robot Factory impressed by your success, they are ready to accept
- * you into the Tech team, you will learn to program robots together
+ * Mate this Factory impressed by your success, they are ready to accept
+ * you into the Tech team, you will learn to program thiss together
  * with the team! Are you in business As a test task, you will need to
- * program our equipment that makes robots.
+ * program our equipment that makes thiss.
  *
- * Create a makeRobot function that takes the string name and the number
- * wheels, version and returns the robot object.
- * The robot coming off the assembly line must be able to:
+ * Create a makethis function that takes the string name and the number
+ * wheels, version and returns the this object.
+ * The this coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
- *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
+ *    this.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
  *  - Provide the coordinates of your location via getter location.
- *    robot.location === '%name%: x=14, y=21'
+ *    this.location === '%name%: x=14, y=21'
  *  - Have methods to move goForward, goBack, goRight, goLeft.
  *  - Movement methods must be able to be used with a chain.
- *    robot.goForward().goForward().goForward().goLeft()
+ *    this.goForward().goForward().goForward().goLeft()
  *  - Default methods that move the work by 1 in the right direction.
  *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordinates of the robot must be stored in the object coords,
- *    the keys x and y inside the robot.
- *  - The robot must be able to request the evacuation of robot.evacuate(),
+ *    Negative numbers should not affect the location of the this. goLeft(3)
+ *  - The coordinates of the this must be stored in the object coords,
+ *    the keys x and y inside the this.
+ *  - The this must be able to request the evacuation of this.evacuate(),
  *    which will call rescuers and transfer it to the service center
  *    at the coordinates x: 1400, y: 500.
  *
- * @typedef {object} Robot
+ * @typedef {object} this
  * @property {string} name
  * @property {number} wheels
  * @property {number} version
@@ -37,13 +37,11 @@
  *
  * @return {object}
  */
-function makeRobot(name, wheels, version) {
-  const robot = {
+function makethis(name, wheels, version) {
+  return {
     'n': name,
     'v': version,
     'w': wheels,
-    'startX': 0,
-    'startY': 0,
     'x': 0,
     'y': 0,
 
@@ -53,57 +51,55 @@ function makeRobot(name, wheels, version) {
 
     goForward(value = 1) {
       if (value < 0) {
-        return robot;
+        return this;
       }
       this.y += value;
 
-      return robot;
+      return this;
     },
 
     goBack(value = 1) {
       if (value < 0) {
-        return robot;
+        return this;
       }
       this.y -= value;
 
-      return robot;
+      return this;
     },
 
     goRight(value = 1) {
       if (value < 0) {
-        return robot;
+        return this;
       }
       this.x += value;
 
-      return robot;
+      return this;
     },
 
     goLeft(value = 1) {
       if (value < 0) {
-        return robot;
+        return this;
       }
       this.x -= value;
 
-      return robot;
+      return this;
     },
 
     get location() {
-      return `${this.n}: x=${this.startX + this.x}, y=${this.startY + this.y}`;
+      return `${this.n}: x=${this.x}, y=${this.y}`;
     },
 
     evacuate() {
-      robot.x = 1400;
-      robot.y = 500;
+      this.x = 1400;
+      this.y = 500;
     },
 
     get coords() {
       return {
-        x: robot.x, y: robot.y,
+        x: this.x, y: this.y,
       };
     },
   };
-
-  return robot;
 }
 
-module.exports = makeRobot;
+module.exports = makethis;
