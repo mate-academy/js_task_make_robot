b0824b3d4d72a9ae1ca86c9391a936ea104bb45d
Tetiana Zotova <zotova.tetiana@gmail.com>
2020-12-15 17:05:25 +0200

    Update src/makeRobot.js
    Co-authored-by: Den <37452763+Denchepornyuk@users.noreply.github.com>

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -41,9 +41,9 @@ function makeRobot(name, wheels, version) {
   // write code here
 
   const robot = {
-    name: name,
-    version: version,
-    wheels: wheels,
+    name,
+    version,
+    wheels,
     coords: {
       x: 0,
       y: 0,
