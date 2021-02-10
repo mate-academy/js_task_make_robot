af301222e2b21a2aa5e474a4a074535a67335c23
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-10 11:36:22 +0200

    first commit

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,6 +39,78 @@
  */
 function makeRobot(name, wheels, version) {
   // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(step) {
+      if (!step) {
+        this.coords.y++;
+      }
+
+      if (step > 0) {
+        this.coords.y += step;
+      }
+
+      return this;
+    },
+
+    goBack(step) {
+      if (!step) {
+        this.coords.y--;
+      }
+
+      if (step > 0) {
+        this.coords.y -= step;
+      }
+
+      return this;
+    },
+
+    goRight(step) {
+      if (!step) {
+        this.coords.x++;
+      }
+
+      if (step > 0) {
+        this.coords.x += step;
+      }
+
+      return this;
+    },
+
+    goLeft(step) {
+      if (!step) {
+        this.coords.x--;
+      }
+
+      if (step > 0) {
+        this.coords.x -= step;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
