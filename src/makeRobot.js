22ea8054c588f171ad0ada64fa0c3e72e619e4bc
Kostrica <romiorw@ukr.net>
2021-03-04 20:49:23 +0200

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,82 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const Robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    goBack(value) {
+      if (value === undefined) {
+        this.coords.y--;
+      } else if (value < 0) {
+        this.coords.y = this.coords.y;
+      } else {
+        this.coords.y = this.coords.y - value;
+      }
+
+      return this;
+    },
+
+    goForward(value) {
+      if (value === undefined) {
+        this.coords.y++;
+      } else if (value < 0) {
+        this.coords.y = this.coords.y;
+      } else {
+        this.coords.y = this.coords.y + value;
+      }
+
+      return this;
+    },
+
+    goLeft(value) {
+      if (value === undefined) {
+        this.coords.x--;
+      } else if (value < 0) {
+        this.coords.x = this.coords.x;
+      } else {
+        this.coords.x = this.coords.x - value;
+      }
+
+      return this;
+    },
+
+    goRight(value) {
+      if (value === undefined) {
+        this.coords.x++;
+      } else if (value < 0) {
+        this.coords.x = this.coords.x;
+      } else {
+        this.coords.x = this.coords.x + value;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return this;
+    },
+
+    get info() {
+      return `name: ${this.name},`
+      + ` chip version: ${this.version},`
+      + ` wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+  };
+
+  return Robot;
 }
 
 module.exports = makeRobot;
