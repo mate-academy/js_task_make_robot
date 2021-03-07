18fac80b36e62da52462d9a06ce2fcdf2a21505f
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-03-08 01:23:47 +0200

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,6 +39,95 @@
  */
 function makeRobot(name, wheels, version) {
   // write code here
+  const robot = {
+    nameRobot: name,
+    wheelsRobot: wheels,
+    versionRobot: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      const stringName = `name: ${this.nameRobot}`;
+      const stringVersion = `chip version: ${this.versionRobot}`;
+      const stringWheels = `wheels: ${this.wheelsRobot}`;
+
+      return `${stringName}, ${stringVersion}, ${stringWheels}`;
+    },
+    get location() {
+      return `${this.nameRobot}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+    goForward(valueY) {
+      if (valueY < 1) {
+        return this;
+      }
+
+      if (valueY >= 1) {
+        this.coords.y += valueY;
+      }
+
+      if (typeof valueY === 'undefined') {
+        this.coords.y += 1;
+      }
+
+      return this;
+    },
+    goBack(valueY) {
+      if (valueY < 1) {
+        return this;
+      }
+
+      if (valueY >= 1) {
+        this.coords.y -= valueY;
+      }
+
+      if (typeof valueY === 'undefined') {
+        this.coords.y -= 1;
+      }
+
+      return this;
+    },
+    goRight(valueX) {
+      if (valueX < 1) {
+        return this;
+      }
+
+      if (valueX >= 1) {
+        this.coords.x += valueX;
+      }
+
+      if (typeof valueX === 'undefined') {
+        this.coords.x += 1;
+      }
+
+      return this;
+    },
+    goLeft(valueX) {
+      if (valueX < 1) {
+        return this;
+      }
+
+      if (valueX >= 1) {
+        this.coords.x -= valueX;
+      }
+
+      if (typeof valueX === 'undefined') {
+        this.coords.x -= 1;
+      }
+
+      return this;
+    },
+    evacuate: function() {
+      const evacuateX = 1400;
+      const evacuateY = 500;
+
+      this.coords.x = evacuateX;
+      this.coords.y = evacuateY;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
