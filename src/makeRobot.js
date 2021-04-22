f93cc4af4edd336728efa824b8d2b7b0fa7e46ea
Anton Kostylov <antoniok003@gmail.com>
2021-04-22 11:57:36 +0300

    fixes according to mentor rewiev

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -40,19 +40,16 @@
 function makeRobot(name, wheels, version) {
   // write code here
   const robot = {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
     coords: {
       x: 0,
       y: 0,
     },
     get info() {
-      const id = this.name;
-      const ver = this.version;
-      const basis = this.wheels;
-
-      return `name: ${id}, chip version: ${ver}, wheels: ${basis}`;
+      return `name: ${this.name},`
+      + ` chip version: ${this.version}, wheels: ${this.wheels}`;
     },
     get location() {
       return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
@@ -62,34 +59,30 @@ function makeRobot(name, wheels, version) {
       this.coords.y = 500;
     },
     goForward(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y += value;
       }
-      this.coords.y += value;
 
       return this;
     },
     goBack(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y -= value;
       }
-      this.coords.y -= value;
 
       return this;
     },
     goRight(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x += value;
       }
-      this.coords.x += value;
 
       return this;
     },
     goLeft(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x -= value;
       }
-      this.coords.x -= value;
 
       return this;
     },
