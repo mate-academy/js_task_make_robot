26b8257909183f94c45d9dbff48d47ce9735c9e3
Igor Lisogor <illisogori@gmail.com>
2021-04-23 15:27:43 +0300

    makeRobot implemented

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,61 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    chipVersion: version,
+    get info() {
+      return `name: ${this.name}, chip version: `
+      + `${this.chipVersion}, wheels: ${this.wheels}`;
+    },
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    get location() {
+      return `${this.name}: x=${this.coords.x}, `
+      + `y=${this.coords.y}`;
+    },
+    goRight(steps = 1) {
+      if (steps < 0) {
+        return this;
+      }
+      this.coords.x += steps;
+
+      return this;
+    },
+    goLeft(steps = 1) {
+      if (steps < 0) {
+        return this;
+      }
+      this.coords.x -= steps;
+
+      return this;
+    },
+    goBack(steps = 1) {
+      if (steps < 0) {
+        return this;
+      }
+      this.coords.y -= steps;
+
+      return this;
+    },
+    goForward(steps = 1) {
+      if (steps < 0) {
+        return this;
+      }
+      this.coords.y += steps;
+
+      return this;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
