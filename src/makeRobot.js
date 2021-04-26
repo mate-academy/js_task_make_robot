6ccb7214ee8ac00e95cd6358018d3df5d4b54e43
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-04-26 16:19:50 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -1,29 +1,6 @@
 'use strict';
 
 /**
- * Mate Robot Factory impressed by your success, they are ready to accept
- * you into the Tech team, you will learn to program robots together
- * with the team! Are you in business As a test task, you will need to
- * program our equipment that makes robots.
- *
- * Create a makeRobot function that takes the string name and the number
- * wheels, version and returns the robot object.
- * The robot coming off the assembly line must be able to:
- *  - Provide information about yourself through getter info.
- *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
- *  - Provide the coords of your location via getter location.
- *    robot.location === '%name%: x=14, y=21'
- *  - Have methods to step goForward, goBack, goRight, goLeft.
- *  - stepment methods must be able to be used with a chain.
- *    robot.goForward().goForward().goForward().goLeft()
- *  - Default methods that step the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coords of the robot must be stored in the object coords,
- *    the keys x and y inside the robot.
- *  - The robot must be able to request the evacuation of robot.evacuate(),
- *    which will call rescuers and transfer it to the service center
- *    at the coords x: 1400, y: 500.
  *
  * @typedef {object} Robot
  * @property {string} name
