703b6fe7df4817527cea8cad24f011d767f130b8
Yurii Lisovenko <yriyliso@gmail.com>
2021-04-27 14:00:54 +0300

    fixes

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -48,11 +48,9 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      return `
-      name: ${this.name}, 
-      chip version: ${this.version}, 
-      wheels: ${this.wheels}
-      `;
+      return `name: ${this.name}, `
+        + `chip version: ${this.version}, `
+        + `wheels: ${this.wheels}`;
     },
 
     get location() {
