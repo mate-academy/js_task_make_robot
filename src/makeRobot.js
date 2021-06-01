20a73487844a28e380c42ceb521a4a011cb75176
Ihor <igor.pyliak@techmagic.co>
2021-06-01 11:44:28 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,95 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0, y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name},`
+      + ` chip version: ${this.version},`
+      + ` wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(step) {
+      if (typeof step !== 'undefined' && step > 0) {
+        this.coords.y += step;
+
+        return this;
+      }
+
+      if (step < 0) {
+        return this;
+      }
+
+      this.coords.y += 1;
+
+      return this;
+    },
+
+    goBack(step) {
+      if (typeof step !== 'undefined' && step > 0) {
+        this.coords.y -= step;
+
+        return this;
+      }
+
+      if (step < 0) {
+        return this;
+      }
+
+      this.coords.y -= 1;
+
+      return this;
+    },
+
+    goRight(step) {
+      if (typeof step !== 'undefined' && step > 0) {
+        this.coords.x += step;
+
+        return this;
+      }
+
+      if (step < 0) {
+        return this;
+      }
+
+      this.coords.x += 1;
+
+      return this;
+    },
+
+    goLeft(step) {
+      if (typeof step !== 'undefined' && step > 0) {
+        this.coords.x -= step;
+
+        return this;
+      }
+
+      if (step < 0) {
+        return this;
+      }
+
+      this.coords.x -= 1;
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
