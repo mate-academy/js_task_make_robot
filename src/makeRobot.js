fb5e227e3653a88e497047101ee60f8c233c88bf
vdzhyranov <v.dzhyranov@gmail.com>
2021-06-18 13:05:27 +0300

    robot

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -1,44 +1,81 @@
 'use strict';
 
 /**
- * Mate Robot Factory impressed by your success, they are ready to accept
- * you into the Tech team, you will learn to program robots together
- * with the team! Are you in business As a test task, you will need to
- * program our equipment that makes robots.
- *
- * Create a makeRobot function that takes the string name and the number
- * wheels, version and returns the robot object.
- * The robot coming off the assembly line must be able to:
- *  - Provide information about yourself through getter info.
- *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
- *  - Provide the coordinates of your location via getter location.
- *    robot.location === '%name%: x=14, y=21'
- *  - Have methods to move goForward, goBack, goRight, goLeft.
- *  - Movement methods must be able to be used with a chain.
- *    robot.goForward().goForward().goForward().goLeft()
- *  - Default methods that move the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordinates of the robot must be stored in the object coords,
- *    the keys x and y inside the robot.
- *  - The robot must be able to request the evacuation of robot.evacuate(),
- *    which will call rescuers and transfer it to the service center
- *    at the coordinates x: 1400, y: 500.
- *
  * @typedef {object} Robot
  * @property {string} name
  * @property {number} wheels
  * @property {number} version
  * @property {function} info
- *
  * @param {string} name
  * @param {number} wheels
  * @param {number} version
- *
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const moveRobot = {
+    x: 0,
+    y: 0,
+  };
+
+  const robot = {
+    name,
+    wheels,
+    version,
+    moveRobot,
+
+    info: `name: ${name}, chip version: ${version}, wheels: ${wheels}`,
+
+    location: `${name}: x=${moveRobot.x}, y=${moveRobot.y}`,
+
+    goRight(step = 1) {
+      if (step < 1) {
+        this.location = this.location;
+      } else {
+        this.location = `${name}: x=${moveRobot.x += step}, y=${moveRobot.y}`;
+      }
+
+      return robot;
+    },
+
+    goLeft(step = 1) {
+      if (step < 1) {
+        this.location = this.location;
+      } else {
+        this.location = `${name}: x=${moveRobot.x -= step}, y=${moveRobot.y}`;
+      }
+
+      return robot;
+    },
+
+    goBack(step = 1) {
+      if (step < 1) {
+        this.location = this.location;
+      } else {
+        this.location = `${name}: x=${moveRobot.x}, y=${moveRobot.y -= step}`;
+      }
+
+      return robot;
+    },
+
+    goForward(step = 1) {
+      if (step < 1) {
+        this.location = this.location;
+      } else {
+        this.location = `${name}: x=${moveRobot.x}, y=${moveRobot.y += step}`;
+      }
+
+      return robot;
+    },
+
+    evacuate() {
+      moveRobot.x = 1400;
+      moveRobot.y = 500;
+
+      return moveRobot;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
