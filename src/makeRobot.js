564e850b517271ade607e5b3bde6f2f9e51fcd5b
David Dohru <tronsmartmy@gmail.com>
2021-06-22 10:58:46 +0300

    done

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -11,7 +11,7 @@
  * The robot coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
  *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
- *  - Provide the coordinates of your location via getter location.
+ *  - Provide the coordss of your location via getter location.
  *    robot.location === '%name%: x=14, y=21'
  *  - Have methods to move goForward, goBack, goRight, goLeft.
  *  - Movement methods must be able to be used with a chain.
@@ -19,11 +19,11 @@
  *  - Default methods that move the work by 1 in the right direction.
  *    This value can be increased by passing the desired number to the method.
  *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordinates of the robot must be stored in the object coords,
+ *  - The coordss of the robot must be stored in the object coords,
  *    the keys x and y inside the robot.
  *  - The robot must be able to request the evacuation of robot.evacuate(),
  *    which will call rescuers and transfer it to the service center
- *    at the coordinates x: 1400, y: 500.
+ *    at the coordss x: 1400, y: 500.
  *
  * @typedef {object} Robot
  * @property {string} name
@@ -38,7 +38,94 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    evacuate: function() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return robot;
+    },
+    name,
+    wheels,
+    version,
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+    goRight: function(value) {
+      if (typeof (value) === 'number' && value > 0) {
+        this.coords.x = this.coords.x + value;
+      }
+
+      if (value < 0) {
+        this.coords.x += 0;
+      }
+
+      if (value === undefined) {
+        this.coords.x++;
+      }
+
+      return robot;
+    },
+
+    goLeft: function(value) {
+      if (typeof (value) === 'number' && value > 0) {
+        this.coords.x = this.coords.x - value;
+      }
+
+      if (value < 0) {
+        this.coords.x += 0;
+      }
+
+      if (value === undefined) {
+        this.coords.x--;
+      }
+
+      return robot;
+    },
+
+    goForward: function(value) {
+      if (typeof (value) === 'number' && value > 0) {
+        this.coords.y = this.coords.y + value;
+      }
+
+      if (value < 0) {
+        this.coords.y += 0;
+      }
+
+      if (value === undefined) {
+        this.coords.y++;
+      }
+
+      return robot;
+    },
+
+    goBack: function(value) {
+      if (value < 0) {
+        this.coords.y += 0;
+      }
+
+      if (typeof (value) === 'number' && value > 0) {
+        this.coords.y = this.coords.y - value;
+      }
+
+      if (value === undefined) {
+        this.coords.y--;
+      }
+
+      return robot;
+    },
+
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
