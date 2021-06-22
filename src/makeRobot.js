be4502df5c46434573fb3143c8648a3642fcd29d
vlepekha <vlepekha@student.ucode.world>
2021-06-23 00:24:58 +0300

    solved

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,54 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    info: `name: ${name}, chip version: ${version}, wheels: ${wheels}`,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+    goRight(steps = 1) {
+      if (steps < 0) {
+        return this;
+      }
+      this.coords.x += steps;
+
+      return this;
+    },
+    goLeft(step = 1) {
+      if (step < 0) {
+        return this;
+      }
+      this.coords.x -= step;
+
+      return this;
+    },
+    goBack(step = 1) {
+      if (step < 0) {
+        return this;
+      }
+      this.coords.y -= step;
+
+      return this;
+    },
+    goForward(step = 1) {
+      if (step < 0) {
+        return this;
+      }
+      this.coords.y += step;
+
+      return this;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
