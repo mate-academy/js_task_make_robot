b083ec3b781a5057cedddcaa903353daf5af3618
sergei-trofimov <feast.st@gmail.com>
2021-06-25 11:45:46 +0300

    Fixed solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -40,7 +40,7 @@
 function makeRobot(name, wheels, version) {
   // write code here
   const robot = {
-    'coords': {
+    coords: {
       'x': 0,
       'y': 0,
     },
@@ -51,34 +51,30 @@ function makeRobot(name, wheels, version) {
       return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
     goBack(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y -= value;
       }
-      this.coords.y -= value;
 
       return this;
     },
     goForward(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y += value;
       }
-      this.coords.y += value;
 
       return this;
     },
     goLeft(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x -= value;
       }
-      this.coords.x -= value;
 
       return this;
     },
     goRight(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x += value;
       }
-      this.coords.x += value;
 
       return this;
     },
