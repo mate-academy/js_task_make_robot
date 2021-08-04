c3b1d1b8e0a90566aa53ea73ce4719e2f018f74d
NastyaVovk <winc4ester@gmail.com>
2021-08-04 14:12:42 +0300

    initial

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,68 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
-}
+  const robot = {
+    _name: name,
+    _wheels: wheels,
+    _version: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this._name},`
+      + ` chip version: ${this._version},`
+      + ` wheels: ${this._wheels}`;
+    },
+
+    get location() {
+      return `${this._name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(value = 1) {
+      if (value <= 0 || typeof value !== 'number') {
+        return this;
+      }
+      this.coords.y += value;
+
+      return this;
+    },
+
+    goBack(value = 1) {
+      if (value <= 0 || typeof value !== 'number') {
+        return this;
+      }
+      this.coords.y -= value;
+
+      return this;
+    },
+
+    goRight(value = 1) {
+      if (value <= 0 || typeof value !== 'number') {
+        return this;
+      }
+      this.coords.x += value;
+
+      return this;
+    },
+
+    goLeft(value = 1) {
+      if (value <= 0 || typeof value !== 'number') {
+        return this;
+      }
+      this.coords.x -= value;
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
+};
 
 module.exports = makeRobot;
