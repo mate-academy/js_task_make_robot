e689c57b64e6fe6c23d655ba1297a8f319d13fa5
Alex Hlotov <alexhlotov67@gmail.com>
2021-08-04 12:29:16 +0300

    Little fix

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -40,9 +40,9 @@
 function makeRobot(name, wheels, version) {
   // write code here
   const robot = {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
     coords: {
       x: 0,
       y: 0,
