54e6a64835a6f9d9b767aef444851746d11ce67e
NastyaVovk <winc4ester@gmail.com>
2021-08-04 15:42:48 +0300

    movement functions chnged

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -58,37 +58,33 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(value = 1) {
-      if (value <= 0 || typeof value !== 'number') {
-        return this;
+      if (value > 0) {
+        this.coords.y += value;
       }
-      this.coords.y += value;
 
       return this;
     },
 
     goBack(value = 1) {
-      if (value <= 0 || typeof value !== 'number') {
-        return this;
+      if (value > 0) {
+        this.coords.y -= value;
       }
-      this.coords.y -= value;
 
       return this;
     },
 
     goRight(value = 1) {
-      if (value <= 0 || typeof value !== 'number') {
-        return this;
+      if (value > 0) {
+        this.coords.x += value;
       }
-      this.coords.x += value;
 
       return this;
     },
 
     goLeft(value = 1) {
-      if (value <= 0 || typeof value !== 'number') {
-        return this;
+      if (value > 0) {
+        this.coords.x -= value;
       }
-      this.coords.x -= value;
 
       return this;
     },
