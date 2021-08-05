b33f483e40ed8c30563a33362aa789a8bad92945
Andrii Hryhoruk <andriihryhoruk5544@gmail.com>
2021-08-05 20:37:27 +0300

    save

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,62 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  return {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, `
+      + `chip version: ${this.version}, `
+      + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(coordinate = 1) {
+      if (coordinate > 0) {
+        this.coords.y += coordinate;
+      }
+
+      return this;
+    },
+
+    goBack(coordinate = 1) {
+      if (coordinate > 0) {
+        this.coords.y -= coordinate;
+      }
+
+      return this;
+    },
+
+    goRight(coordinate = 1) {
+      if (coordinate > 0) {
+        this.coords.x += coordinate;
+      }
+
+      return this;
+    },
+
+    goLeft(coordinate = 1) {
+      if (coordinate > 0) {
+        this.coords.x -= coordinate;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
 }
 
 module.exports = makeRobot;
