f3b2b248f0ac5ea4f16bb949865a9b040fad5199
Rak Oleg <rakole86@gmail.com>
2021-09-10 16:04:37 +0300

    Make Robot

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -37,8 +37,59 @@
  *
  * @return {object}
  */
+
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    'name': name,
+    'chip version': version,
+    'wheels': wheels,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+    get info() {
+      return `name: ${this.name}, \
+chip version: ${version}, \
+wheels: ${this.wheels}`;
+    },
+    goRight(value = 1) {
+      if (value > 0) {
+        this.coords.x += value;
+      }
+
+      return robot;
+    },
+    goLeft(value = 1) {
+      if (value > 0) {
+        this.coords.x -= value;
+      }
+
+      return robot;
+    },
+    goBack(value = 1) {
+      if (value > 0) {
+        this.coords.y -= value;
+      }
+
+      return robot;
+    },
+    goForward(value = 1) {
+      if (value > 0) {
+        this.coords.y += value;
+      }
+
+      return robot;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
