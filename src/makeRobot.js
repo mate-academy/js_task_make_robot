88aadc2e7b0dddaafa02a1c420a1232533402d55
Rak Oleg <rakole86@gmail.com>
2021-09-13 16:34:21 +0300

    makeRobot fixed

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -40,48 +40,50 @@
 
 function makeRobot(name, wheels, version) {
   const robot = {
-    'name': name,
-    'chip version': version,
-    'wheels': wheels,
+    name,
+    version,
+    wheels,
     coords: {
       x: 0,
       y: 0,
     },
+
     get location() {
       return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
+
     get info() {
-      return `name: ${this.name}, \
-chip version: ${version}, \
-wheels: ${this.wheels}`;
+      return `name: ${this.name}, `
+      + `chip version: ${version}, `
+      + `wheels: ${this.wheels}`;
     },
     goRight(value = 1) {
       if (value > 0) {
         this.coords.x += value;
       }
 
-      return robot;
+      return this;
     },
     goLeft(value = 1) {
       if (value > 0) {
         this.coords.x -= value;
       }
 
-      return robot;
+      return this;
     },
     goBack(value = 1) {
       if (value > 0) {
         this.coords.y -= value;
       }
 
-      return robot;
+      return this;
     },
     goForward(value = 1) {
       if (value > 0) {
         this.coords.y += value;
       }
 
-      return robot;
+      return this;
     },
     evacuate() {
       this.coords.x = 1400;
