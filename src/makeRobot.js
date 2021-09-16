3a8d70bf8701b1616dd0554836e859f701d75eb3
AVronskyi <vronskyi.a@gmail.com>
2021-09-16 16:36:56 +0300

    solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -19,7 +19,7 @@
  *  - Default methods that move the work by 1 in the right direction.
  *    This value can be increased by passing the desired number to the method.
  *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordinates of the robot must be stored in the object coordinates,
+ *  - The coordinates of the robot must be stored in the object coords,
  *    the keys x and y inside the robot.
  *  - The robot must be able to request the evacuation of robot.evacuate(),
  *    which will call rescuers and transfer it to the service center
