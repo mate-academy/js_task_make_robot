f21d58126c7c6991b7f3d7725702f58b671f82ec
Victor Shevchuk <callmevitok@gmail.com>
2021-09-17 09:37:10 +0300

    robot

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -57,28 +57,28 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(step = 1) {
-      if (step >= 0) {
+      if (step > 0) {
         this.coords.y += step;
       }
 
       return this;
     },
     goBack(step = 1) {
-      if (step >= 0) {
+      if (step > 0) {
         this.coords.y -= step;
       }
 
       return this;
     },
     goRight(step = 1) {
-      if (step >= 0) {
+      if (step > 0) {
         this.coords.x += step;
       }
 
       return this;
     },
     goLeft(step = 1) {
-      if (step >= 0) {
+      if (step > 0) {
         this.coords.x -= step;
       }
 
