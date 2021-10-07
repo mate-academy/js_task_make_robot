7af1ae17b1c7b66b306e5d4bf56ac306ac1a83c4
Виталий Пасечник <47535101+VitalyPasechnik@users.noreply.github.com>
2021-10-08 00:37:45 +0300

    Update makeRobot.js

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,67 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  return name;
+    const robot = {
+    name,
+    wheels,
+    version,
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, chip version: ${
+        this.version}, wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goBack(value = 1) {
+      if (value >= 1) {
+        this.coords.y -= value;
+      }
+
+      return this;
+    },
+
+    goForward(value = 1) {
+      if (value >= 1) {
+        this.coords.y += value;
+      }
+
+      return this;
+    },
+
+    goLeft(value = 1) {
+      if (value >= 1) {
+        this.coords.x -= value;
+      }
+
+      return this;
+    },
+
+    goRight(value = 1) {
+      if (value >= 1) {
+        this.coords.x += value;
+      };
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords = {
+        x: 1400, y: 500,
+      };
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
