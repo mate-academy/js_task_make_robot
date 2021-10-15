a108ae085fada2d1e03b5e814b8d2b0d65e2cf5f
Konstantin <ivanchenkomypost@gmail.com>
2021-10-15 20:38:22 +0300

    task is done

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,63 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, chip version: ${this.version}, `
+        + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(arg = 1) {
+      if (arg > 0) {
+        this.coords.y += arg;
+      }
+
+      return this;
+    },
+
+    goBack(arg = 1) {
+      if (arg > 0) {
+        this.coords.y -= arg;
+      }
+
+      return this;
+    },
+
+    goRight(arg = 1) {
+      if (arg > 0) {
+        this.coords.x += arg;
+      }
+
+      return this;
+    },
+
+    goLeft(arg = 1) {
+      if (arg > 0) {
+        this.coords.x -= arg;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
