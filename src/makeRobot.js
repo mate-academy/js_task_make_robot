474f5dfa3715758ebc9edb0e1a9d4f695727224e
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2021-11-11 14:27:38 +0200

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,76 @@
  * @return {object}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(value = 1) {
+      if (value > 0) {
+        this.coords.y += value;
+      }
+
+      return this;
+    },
+
+    goBack(value) {
+      if (typeof value === 'undefined') {
+        this.coords.y--;
+      }
+
+      if (typeof value === 'number' && value > 0) {
+        this.coords.y -= value;
+      }
+
+      return this;
+    },
+
+    goRight(value) {
+      if (typeof value === 'undefined') {
+        this.coords.x++;
+      }
+
+      if (typeof value === 'number' && value > 0) {
+        this.coords.x += value;
+      }
+
+      return this;
+    },
+
+    goLeft(value) {
+      if (typeof value === 'undefined') {
+        this.coords.x--;
+      }
+
+      if (typeof value === 'number' && value > 0) {
+        this.coords.x -= value;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
