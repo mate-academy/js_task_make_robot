ae2d5981fcb18b2777eab5d95cace5e72093fecd
Juliya <jursylian@gmail.com>
2022-01-24 14:04:45 +0200

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -48,8 +48,8 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      return `name:
-      ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+      return `name: ${this.name}, chip version: ${this.version},`
+      + `wheels: ${this.wheels}`;
     },
 
     get location() {
