a9fadbe3b74f24f9a1a0cb34570ecef3089f3433
Kostiantyn <havryshkostya@gmail.com>
2022-04-09 12:52:24 +0400

    I like(no) your robots tasks

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,57 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    x: 0,
+    y: 0,
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(value = 1) {
+      if (value > 0) {
+        this.coords.y = this.coords.y + value;
+      }
+
+      return this;
+    },
+    goBack(value = 1) {
+      if (value > 0) {
+        this.coords.y = this.coords.y - value;
+      }
+
+      return this;
+    },
+    goRight(value = 1) {
+      if (value > 0) {
+        this.coords.x = this.coords.x + value;
+      }
+
+      return this;
+    },
+    goLeft(value = 1) {
+      if (value > 0) {
+        this.coords.x = this.coords.x - value;
+      }
+
+      return this;
+    },
+    coords: {
+      x: 0,
+      y: 0,
+
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
