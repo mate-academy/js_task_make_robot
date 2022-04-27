93c843e280f618b026c21e435c1e9a6186125011
Lebogang Mogane <mogane420@gmail.com>
2022-04-27 06:17:07 +0200

    remove this keyword from getter info

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -48,8 +48,7 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      /* eslint-disable */
-      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
     },
 
     goForward(y = 1) {
