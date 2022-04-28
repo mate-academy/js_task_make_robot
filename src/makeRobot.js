2223700cdd4b6c4aed022dd4e70db019f6b89f2f
Ihor Yeremenko <igoree86@gmail.com>
2022-04-28 15:34:56 +0300

    First_commit_Make_Robot

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,66 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, `
+      + `chip version: ${this.version}, `
+      + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: `
+      + `x=${this.coords.x}, `
+      + `y=${this.coords.y}`;
+    },
+
+    goBack(c = 1) {
+      if (c >= 0) {
+        this.coords.y -= c;
+      }
+
+      return this;
+    },
+
+    goForward(c = 1) {
+      if (c >= 0) {
+        this.coords.y += c;
+      }
+
+      return this;
+    },
+
+    goLeft(c = 1) {
+      if (c >= 0) {
+        this.coords.x -= c;
+      }
+
+      return this;
+    },
+
+    goRight(c = 1) {
+      if (c >= 0) {
+        this.coords.x += c;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
