cbbb3e3895904c03c0e94b10f13c9745a15b6215
Daryna Chekmariova <dashachekmariova@gmail.com>
2022-04-28 21:35:07 +0300

    make robot

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,64 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return ('name: ' + name
+        + ', chip version: ' + version
+        + ', wheels: ' + wheels);
+    },
+
+    goForward(step = 1) {
+      if (step > 0) {
+        robot.coords.y += step;
+      }
+
+      return this;
+    },
+
+    goBack(step = 1) {
+      if (step > 0) {
+        robot.coords.y -= step;
+      }
+
+      return this;
+    },
+
+    goRight(step = 1) {
+      if (step > 0) {
+        robot.coords.x += step;
+      }
+
+      return this;
+    },
+
+    goLeft(step = 1) {
+      if (step > 0) {
+        robot.coords.x -= step;
+      }
+
+      return this;
+    },
+
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    evacuate() {
+      robot.coords.x = 1400;
+      robot.coords.y = 500;
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
