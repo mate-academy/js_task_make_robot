293db7db9bd8af7031dacc15cb09d21b6b466c99
Oleksandr Ivanchenko <aleksandrod92@gmil.com>
2022-04-28 22:42:29 +0300

    robotMove

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -11,11 +11,14 @@
  * The robot coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
  *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
+ *
  *  - Provide the coordinates of your location via getter location.
  *    robot.location === '%name%: x=14, y=21'
+ *
  *  - Have methods to move goForward, goBack, goRight, goLeft.
  *  - Movement methods must be able to be used with a chain.
  *    robot.goForward().goForward().goForward().goLeft()
+ *
  *  - Default methods that move the work by 1 in the right direction.
  *    This value can be increased by passing the desired number to the method.
  *    Negative numbers should not affect the location of the robot. goLeft(3)
@@ -38,7 +41,57 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    get info() {
+      return `name: ${this.name}, chip version: ${
+        this.version}, wheels: ${this.wheels}`;
+    },
+    get location() {
+      return `${this.name}: x=${
+        this.coords.x}, y=${this.coords.y}`;
+    },
+    goForward: function(number = 1) {
+      if (number > 0) {
+        this.coords.y += number;
+      }
+
+      return this;
+    },
+    goBack: function(number = 1) {
+      if (number > 0) {
+        this.coords.y -= number;
+      }
+
+      return this;
+    },
+    goRight: function(number = 1) {
+      if (number > 0) {
+        this.coords.x += number;
+      }
+
+      return this;
+    },
+    goLeft: function(number = 1) {
+      if (number > 0) {
+        this.coords.x -= number;
+      }
+
+      return this;
+    },
+    evacuate: function() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
