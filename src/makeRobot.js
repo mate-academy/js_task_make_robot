9e9777991e870237c165557b5a56ff9fffc9b1de
Dmytro Kaunin <kaunin1198@te.net.ua>
2022-05-16 20:43:15 +0300

    First commit / solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,64 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    version,
+    wheels,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, ` + `chip version: ${this.version}, `
+     + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(motion = 1) {
+      if (motion > 0) {
+        this.coords.y += motion;
+      }
+
+      return this;
+    },
+
+    goBack(motion = 1) {
+      if (motion > 0) {
+        this.coords.y -= motion;
+      }
+
+      return this;
+    },
+
+    goLeft(motion = 1) {
+      if (motion > 0) {
+        this.coords.x -= motion;
+      }
+
+      return this;
+    },
+
+    goRight(motion = 1) {
+      if (motion > 0) {
+        this.coords.x += motion;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
