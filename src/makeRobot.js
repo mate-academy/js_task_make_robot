4fe4430b49b4048ebc7ccabb2815b6cf9072b662
Denys Halopa <galyopa@gmail.com>
2022-06-02 21:05:37 +0300

    fixed info

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -47,8 +47,8 @@ function makeRobot(name, wheels, version) {
       y: 0,
     },
     get info() {
-      return `name: ${this.name}, chip version: ${this.version},
-      wheels: ${this.wheels}`;
+      return `name: ${this.name}, chip version: ${this.version},`
+      + ` wheels: ${this.wheels}`;
     },
     get location() {
       return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
