42252de9c181e4919f042e24ac3b67f72435e7ee
Max Dumiak <maxdumiak39@gmail.com>
2022-06-08 14:36:24 +0300

    makeRobot - ready

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,67 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, `
+        + `chip version: ${this.version}, wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goBack(value) {
+      if (value < 0) {
+        return this;
+      }
+      this.coords.y -= value || 1;
+
+      return this;
+    },
+
+    goForward(value) {
+      if (value < 0) {
+        return this;
+      }
+      this.coords.y += value || 1;
+
+      return this;
+    },
+
+    goLeft(value) {
+      if (value < 0) {
+        return this;
+      }
+      this.coords.x -= value || 1;
+
+      return this;
+    },
+
+    goRight(value) {
+      if (value < 0) {
+        return this;
+      }
+      this.coords.x += value || 1;
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
