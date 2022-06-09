172be86c2d16642dd82b1b902f20077d518e3deb
viktorlyt <viktorlyt@gmail.com>
2022-06-09 12:16:11 +0300

    fixed

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -49,7 +49,8 @@ function makeRobot(name, wheels, version) {
 
     get info() {
       // eslint-disable-next-line max-len
-      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+      return `name: ${this.name}, `
+      + `chip version: ${this.version}, wheels: ${this.wheels}`;
     },
 
     get location() {
