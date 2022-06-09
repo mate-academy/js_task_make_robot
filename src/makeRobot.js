b3bd9dcc53399837a2e9dbbb26702a3a91a02e09
Yurii Sydorchuk <victance.3@gmail.com>
2022-06-09 18:28:01 +0300

    fixed line 51

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -44,8 +44,7 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      return `name: ${name}, chip version: ${version}, `
-      + `wheels: ${wheels}`;
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
     },
 
     get location() {
