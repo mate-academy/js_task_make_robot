4c36e1d3257af7b73419f37023c945742d148b8e
Yurii Sydorchuk <victance.3@gmail.com>
2022-06-09 18:11:44 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -1,13 +1,9 @@
 'use strict';
 
 /**
- * Mate Robot Factory impressed by your success, they are ready to accept
- * you into the Tech team, you will learn to program robots together
- * with the team! Are you in business As a test task, you will need to
- * program our equipment that makes robots.
- *
  * Create a makeRobot function that takes the string name and the number
  * wheels, version and returns the robot object.
+
  * The robot coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
  *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
@@ -38,7 +34,63 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, `
+      + `wheels: ${wheels}`;
+    },
+
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+
+    goForward(value = 1) {
+      if (value > 0) {
+        this.coords.y += value;
+      }
+
+      return this;
+    },
+
+    goBack(value = 1) {
+      if (value > 0) {
+        this.coords.y -= value;
+      }
+
+      return this;
+    },
+
+    goRight(value = 1) {
+      if (value > 0) {
+        this.coords.x += value;
+      }
+
+      return this;
+    },
+
+    goLeft(value = 1) {
+      if (value > 0) {
+        this.coords.x -= value;
+      }
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
