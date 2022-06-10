9a17c6d3f3991a147d071cd23a51552f8df19123
Ihor Novakovskyi <igorplan3@gmail.com>
2022-06-10 17:21:28 +0300

    changes

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -40,9 +40,9 @@
 function makeRobot(name, wheels, version) {
   // write code here
   const robot = {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
     x: 0,
     y: 0,
     get coords() {
