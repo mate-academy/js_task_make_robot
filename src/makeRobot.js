48ea9d4d0690117cdaa3a381a1dc2253a7dc9b52
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-07-04 11:23:16 +0300

    refreshed solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -59,7 +59,7 @@ function makeRobot(name, wheels, version) {
     },
 
     get location() {
-      return `Joy: x=${this.coords.x}, y=${this.coords.y}`;
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
 
     goRight(step = 1) {
