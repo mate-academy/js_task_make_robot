31c9898104c83e11f091ac78fd7dd0af8f80458b
Oleg Vetrov <oleg94038@gmail.com>
2022-07-05 21:20:46 +0300

    Added evacuate coordinates in variables

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -47,6 +47,11 @@ function makeRobot(name, wheels, version) {
       y: 0,
     },
 
+    evacuateCoords: {
+      x: 1400,
+      y: 500,
+    },
+
     get info() {
       return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
     },
@@ -88,8 +93,8 @@ function makeRobot(name, wheels, version) {
     },
 
     evacuate() {
-      this.coords.x = 1400;
-      this.coords.y = 500;
+      this.coords.x = this.evacuateCoords.x;
+      this.coords.y = this.evacuateCoords.y;
     },
   };
 
