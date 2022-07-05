7aa44193d5ddab3638fbe3afc8b5c50235a37b5d
Olha Stakhova <olha.stakhova@gmail.com>
2022-07-05 19:47:40 +0300

    solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,95 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, chip version: ${this.version
+      }, wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(yParam) {
+      if (!yParam) {
+        (this.coords.y += 1);
+
+        return this;
+      }
+
+      if (yParam < 0) {
+        return this;
+      }
+
+      this.coords.y += yParam;
+
+      return this;
+    },
+
+    goBack(yParam) {
+      if (!yParam) {
+        (this.coords.y -= 1);
+
+        return this;
+      }
+
+      if (yParam < 0) {
+        return this;
+      }
+
+      this.coords.y -= yParam;
+
+      return this;
+    },
+
+    goRight(xParm) {
+      if (!xParm) {
+        (this.coords.x += 1);
+
+        return this;
+      }
+
+      if (xParm < 0) {
+        return this;
+      }
+
+      this.coords.x += xParm;
+
+      return this;
+    },
+
+    goLeft(xParm) {
+      if (!xParm) {
+        (this.coords.x -= 1);
+
+        return this;
+      }
+
+      if (xParm < 0) {
+        return this;
+      }
+
+      this.coords.x -= xParm;
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
