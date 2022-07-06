45ed12bd0405710169888561f330246f2bf30a62
Olena Rodiontseva <elena.rodiontseva@gmail.com>
2022-07-07 00:16:22 +0200

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -6,22 +6,31 @@
  * with the team! Are you in business As a test task, you will need to
  * program our equipment that makes robots.
  *
- * Create a makeRobot function that takes the string name and the number
- * wheels, version and returns the robot object.
+ *
+ * + Create a makeRobot function that takes the string name and the number
+ *   wheels, version and returns the robot object.
+ *
  * The robot coming off the assembly line must be able to:
- *  - Provide information about yourself through getter info.
+ *
+ *  + Provide information about yourself through getter info.
  *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
- *  - Provide the coordinates of your location via getter location.
+ *
+ *  + Provide the coordinates of your location via getter location.
  *    robot.location === '%name%: x=14, y=21'
- *  - Have methods to move goForward, goBack, goRight, goLeft.
+ *
+ * + The coordinates of the robot must be stored in the object coords,
+ *    the keys x and y inside the robot.
+ *
+ *  + Have methods to move goForward, goBack, goRight, goLeft.
+ *
  *  - Movement methods must be able to be used with a chain.
  *    robot.goForward().goForward().goForward().goLeft()
+ *
  *  - Default methods that move the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordinates of the robot must be stored in the object coords,
- *    the keys x and y inside the robot.
- *  - The robot must be able to request the evacuation of robot.evacuate(),
+ *   - This value can be increased by passing the desired number to the method.
+ *   - Negative numbers should not affect the location of the robot. goLeft(3)
+ *
+ *  + The robot must be able to request the evacuation of robot.evacuate(),
  *    which will call rescuers and transfer it to the service center
  *    at the coordinates x: 1400, y: 500.
  *
@@ -38,7 +47,75 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `\
+name: ${this.name}, \
+chip version: ${this.version}, \
+wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    evacuate() {
+      return `\
+x: ${(this.coords.x += 1400)}, \
+y: ${(this.coords.y += 500)}`;
+    },
+
+    goRight(step = 1) {
+      if (step < 0) {
+        return this;
+      } else {
+        this.coords.x += step;
+      }
+
+      return this;
+    },
+
+    goLeft(step = 1) {
+      if (step < 0) {
+        return this;
+      } else {
+        this.coords.x -= step;
+      }
+
+      return this;
+    },
+
+    goForward(step = 1) {
+      if (step < 0) {
+        return this;
+      } else {
+        this.coords.y += step;
+      }
+
+      return this;
+    },
+
+    goBack(step = 1) {
+      if (step < 0) {
+        return this;
+      } else {
+        this.coords.y -= step;
+      }
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
