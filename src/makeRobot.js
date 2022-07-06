7ecef01ccbdf8820ca8c0ee9c7f15e531e743a78
PavloMarkov <r9markovpn@gmail.com>
2022-07-06 16:26:08 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,6 +39,87 @@
  */
 function makeRobot(name, wheels, version) {
   // write code here
+  const robot = {
+    name,
+    wheels,
+    'chip version': version,
+    get info() {
+      return `name: ${this.name},`
+      + ` chip version: ${this['chip version']},`
+      + ` wheels: ${this.wheels}`;
+    },
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goBack(y = 1) {
+      if (y < 0) {
+        return this;
+      }
+
+      this.location = `Joy: x=0, y=${-y}`;
+
+      return this;
+    },
+
+    goForward(y = 1) {
+      if (y < 0) {
+        return this;
+      }
+      this.location = `Joy: x=0, y=${y}`;
+
+      return this;
+    },
+
+    goRight(x = 1) {
+      if (x < 0) {
+        return this;
+      }
+      this.location = `Joy: x=${x}, y=0`;
+
+      return this;
+    },
+
+    goLeft(x = 1) {
+      if (x < 0) {
+        return this;
+      }
+      this.location = `Joy: x=${-x}, y=0`;
+
+      return this;
+    },
+    set location(value) {
+      if (typeof value !== 'string') {
+        return;
+      }
+
+      const startX = value.lastIndexOf('x');
+      const endX = value.lastIndexOf(',');
+      const coordX = value.slice(startX + 2, endX);
+
+      this.coords.x += Number(coordX);
+
+      const startY = value.lastIndexOf('=');
+      const coordY = value.slice(startY + 1);
+
+      this.coords.y += Number(coordY);
+    },
+
+    evacuate() {
+      const x = 1400 - this.coords.x;
+      const y = 500 - this.coords.y;
+
+      this.location = `Joy: x=${x}, y=${y}`;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
