312cc1a5942ef744b51d44b0ed3b71e1a1c8b564
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-07-06 14:15:56 +0300

    line continuator added

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -49,8 +49,8 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      return `name: ${this.name}, chip version: ${this.version}, wheels: ${
-        this.wheels}`;
+      return `name: ${this.name}, chip version: ${this.version},\
+ wheels: ${this.wheels}`;
     },
 
     get location() {
