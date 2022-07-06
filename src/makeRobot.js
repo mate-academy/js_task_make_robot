7be194e2e46e48727e05432f836337e87587c21b
Sasha Kolomiiets <kinbean444@gmail.com>
2022-07-06 17:33:01 +0300

    added solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -56,38 +56,30 @@ function makeRobot(name, wheels, version) {
     goForward(value = 1) {
       if (value > 0) {
         this.coords.y += value;
-
-        return this;
-      } else {
-        return this;
       }
+
+      return this;
     },
     goBack(value = 1) {
       if (value > 0) {
         this.coords.y -= value;
-
-        return this;
-      } else {
-        return this;
       }
+
+      return this;
     },
     goRight(value = 1) {
       if (value > 0) {
         this.coords.x += value;
-
-        return this;
-      } else {
-        return this;
       }
+
+      return this;
     },
     goLeft(value = 1) {
       if (value > 0) {
         this.coords.x -= value;
-
-        return this;
-      } else {
-        return this;
       }
+
+      return this;
     },
     evacuate() {
       this.coords.x = 1400;
