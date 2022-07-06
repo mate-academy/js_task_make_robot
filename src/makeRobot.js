6e35e3d67fbbfc7601730192ac705625d4acea80
dasha <dashasv76@gmail.com>
2022-07-06 09:00:37 +0300

    serviceCenterCoords was added

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -48,6 +48,11 @@ function makeRobot(name, wheels, version) {
       y: 0,
     },
 
+    serviceCenter: {
+      x: 1400,
+      y: 500,
+    },
+
     get info() {
       return `name: ${this.name}, chip version: ${this.version}, wheels: ${
         this.wheels}`;
@@ -90,8 +95,8 @@ function makeRobot(name, wheels, version) {
     },
 
     evacuate() {
-      this.coords.x = 1400;
-      this.coords.y = 500;
+      this.coords.x = this.serviceCenter.x;
+      this.coords.y = this.serviceCenter.y;
     },
   };
 
