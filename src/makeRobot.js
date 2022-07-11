34817d918f3f20cea515d6b1a9c64ca3213ab9c7
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-07-11 10:59:34 +0300

    functions goSomewhere were fixed

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -60,19 +60,17 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(step = 1) {
-      if (step < 0) {
-        return this;
+      if (step > 0) {
+        this.coords.y += step;
       }
-      this.coords.y += step;
 
       return this;
     },
 
     goBack(step = 1) {
-      if (step < 0) {
-        return this;
+      if (step > 0) {
+        this.coords.y -= step;
       }
-      this.coords.y -= step;
 
       return this;
     },
@@ -86,10 +84,9 @@ function makeRobot(name, wheels, version) {
     },
 
     goLeft(step = 1) {
-      if (step < 0) {
-        return this;
+      if (step > 0) {
+        this.coords.x -= step;
       }
-      this.coords.x -= step;
 
       return this;
     },
