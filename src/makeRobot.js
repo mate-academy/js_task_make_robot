a4a1c4217e7336613b3bbfbbb3231f8f5575f438
ctra <vadym.tsymbal.ua@gmail.com>
2022-07-14 14:52:57 +0300

    solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -6,7 +6,7 @@
  * with the team! Are you in business As a test task, you will need to
  * program our equipment that makes robots.
  *
- * Create a makeRobot function that takes the string name and the number
+ * Create a makeRobot function that takes the string name and the number of
  * wheels, version and returns the robot object.
  * The robot coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
@@ -38,7 +38,65 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+    get location() {
+      return `${name}: x=${robot.coords.x}, y=${robot.coords.y}`;
+    },
+    goForward(step = 1) {
+      if (step >= 0) {
+        this.coords.y += step;
+      } else {
+        return this;
+      }
+
+      return this;
+    },
+    goBack: function(step = 1) {
+      if (step >= 0) {
+        robot.coords.y -= step;
+      } else {
+        return this;
+      }
+
+      return this;
+    },
+    goRight: function(step = 1) {
+      if (step >= 0) {
+        robot.coords.x += step;
+      } else {
+        return this;
+      }
+
+      return this;
+    },
+    goLeft: function(step = 1) {
+      if (step >= 0) {
+        robot.coords.x -= step;
+      } else {
+        return this;
+      }
+
+      return this;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return 'x: this.coords.x, y: this.coords.y';
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
