5f127e2dfaec1a70443a469bf80e4a1c5d92e069
Serhii Yunnikov <yunnikov@icloud.com>
2022-08-09 16:24:03 +0300

    fixed

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,9 +39,9 @@
  */
 function makeRobot(name, wheels, version) {
   const robot = {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
     coords: {
       x: 0,
       y: 0,
@@ -60,40 +60,36 @@ function makeRobot(name, wheels, version) {
       this.coords.x = 1400;
       this.coords.y = 500;
 
-      return this.coords;
+      return this;
     },
     goForward(forward = 1) {
-      if (forward < 0) {
-        return this;
+      if (forward > 0) {
+        this.coords.y += forward;
       }
-      this.coords.y += forward;
 
       return this;
     },
 
     goBack(back = 1) {
-      if (back < 0) {
-        return this;
+      if (back > 0) {
+        this.coords.y -= back;
       }
-      this.coords.y -= back;
 
       return this;
     },
 
     goRight(right = 1) {
-      if (right < 0) {
-        return this;
+      if (right > 0) {
+        this.coords.x += right;
       }
-      this.coords.x += right;
 
       return this;
     },
 
     goLeft(left = 1) {
-      if (left < 0) {
-        return this;
+      if (left > 0) {
+        this.coords.x -= left;
       }
-      this.coords.x -= left;
 
       return this;
     },
