0c875c40669dd59ce5007bcca89eb468209c1395
Sasha Gordijchuk <san4ez1717@gmail.com>
2022-08-10 17:51:39 +0300

    task complette

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,100 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: 'no__name',
+    wheels: 0,
+    version: 0,
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    set robotName(nameValue) {
+      if (typeof nameValue !== 'string') {
+        return;
+      }
+
+      this.name = nameValue;
+    },
+
+    set robotWheels(wheelsValue) {
+      if (typeof wheelsValue !== 'number') {
+        return;
+      }
+
+      this.wheels = wheelsValue;
+    },
+
+    set robotVersion(versionValue) {
+      if (typeof versionValue !== 'number') {
+        return;
+      }
+
+      this.version = versionValue;
+    },
+
+    get info() {
+      // eslint-disable-next-line max-len
+      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(value) {
+      if (!value) {
+        this.coords.y++;
+      } else if (value > 0) {
+        this.coords.y += value;
+      }
+
+      return this;
+    },
+
+    goBack(value) {
+      if (!value) {
+        this.coords.y--;
+      } else if (value > 0) {
+        this.coords.y -= value;
+      }
+
+      return this;
+    },
+
+    goRight(value) {
+      if (!value) {
+        this.coords.x++;
+      } else if (value > 0) {
+        this.coords.x += value;
+      }
+
+      return this;
+    },
+
+    goLeft(value) {
+      if (!value) {
+        this.coords.x--;
+      } else if (value > 0) {
+        this.coords.x -= value;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  robot.robotName = name;
+  robot.robotWheels = wheels;
+  robot.robotVersion = version;
+
+  return robot;
 }
 
 module.exports = makeRobot;
