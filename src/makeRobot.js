4ac85e247630f5fc57fc6264356a46903503e04c
Synytsyn Denys <yungdena54@gmail.com>
2022-08-11 13:26:47 +0300

    fixed solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -48,12 +48,11 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      // eslint-disable-next-line
-      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
     },
 
     get location() {
-      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
 
     evacuate() {
