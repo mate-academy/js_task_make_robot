f148fff1601b51f869aa39d367ddf52667bc2c05
Shahmatov David <davidsahmatov4@gmail.com>
2022-08-15 11:56:52 +0300

    last version

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -43,45 +43,46 @@ function makeRobot(name, wheels, version) {
     wheels,
     version,
 
-    get info() {
-      return `name: ${this.name}, chip version: ${version}, wheels: ${wheels}`;
-    },
-
     coords: {
       x: 0,
       y: 0,
     },
 
+    get info() {
+      return `name: ${this.name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+
     get location() {
       return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
-    goBack(value = 1) {
-      if (value > 0) {
-        this.coords.y -= value;
+
+    goBack(steps = 1) {
+      if (steps > 0) {
+        this.coords.y -= steps;
       }
 
       return this;
     },
 
-    goForward(value = 1) {
-      if (value > 0) {
-        this.coords.y += value;
+    goForward(steps = 1) {
+      if (steps > 0) {
+        this.coords.y += steps;
       }
 
       return this;
     },
 
-    goLeft(value = 1) {
-      if (value > 0) {
-        this.coords.x -= value;
+    goLeft(steps = 1) {
+      if (steps > 0) {
+        this.coords.x -= steps;
       }
 
       return this;
     },
 
-    goRight(value = 1) {
-      if (value > 0) {
-        this.coords.x += value;
+    goRight(steps = 1) {
+      if (steps > 0) {
+        this.coords.x += steps;
       }
 
       return this;
