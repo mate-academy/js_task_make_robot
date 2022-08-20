5e62f43d9087b4bcc25df9869c686339cc0e626b
Vitalii Babiienko <v.babiyenko@gmail.com>
2022-08-20 14:23:47 +0300

    solution fixed

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,16 +39,23 @@
  */
 function makeRobot(name, wheels, version) {
   const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+
     get info() {
       return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
     },
+
     coords: {
       x: 0,
       y: 0,
     },
+
     get location() {
       return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
+
     goForward(step = 1) {
       if (step > 0) {
         this.coords.y += step;
@@ -56,6 +63,7 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
+
     goBack(step = 1) {
       if (step > 0) {
         this.coords.y -= step;
@@ -63,6 +71,7 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
+
     goRight(step = 1) {
       if (step > 0) {
         this.coords.x += step;
@@ -70,6 +79,7 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
+
     goLeft(step = 1) {
       if (step > 0) {
         this.coords.x -= step;
@@ -77,6 +87,7 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
+
     evacuate() {
       this.coords.x = 1400;
       this.coords.y = 500;
