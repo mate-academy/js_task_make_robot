52b54c42091535a4126bbbea6efd47bd70aa8321
miamarichka <ilchyshyn.marichka@gmail.com>
2022-09-09 16:09:28 +0300

    fix

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -51,8 +51,8 @@ function makeRobot(name, wheels, version) {
 
     get info() {
       return `name: ${this.name}`
-      + `, chip version:${this.version}`
-      + `, wheels:${this.wheels}`;
+      + `, ` + `chip version: ${this.version}`
+      + `, ` + `wheels: ${this.wheels}`;
     },
 
     get location() {
