1e2625ea897f1171b35b5947cd09ca1b8d2420b2
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-09-13 22:30:17 +0300

    Solution with changed condition

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -64,37 +64,33 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(steps = 1) {
-      if (steps <= 0) {
-        return this;
+      if (steps > 0) {
+        this.coords.y += steps;
       }
-      this.coords.y += steps;
 
       return this;
     },
 
     goBack(steps = 1) {
-      if (steps <= 0) {
-        return this;
+      if (steps > 0) {
+        this.coords.y -= steps;
       }
-      this.coords.y -= steps;
 
       return this;
     },
 
     goRight(steps = 1) {
-      if (steps <= 0) {
-        return this;
+      if (steps > 0) {
+        this.coords.x += steps;
       }
-      this.coords.x += steps;
 
       return this;
     },
 
     goLeft(steps = 1) {
-      if (steps <= 0) {
-        return this;
+      if (steps > 0) {
+        this.coords.x -= steps;
       }
-      this.coords.x -= steps;
 
       return this;
     },
