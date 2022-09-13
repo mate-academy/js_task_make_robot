89d2e34918deedeef96e82ef270441746667414c
Killdex <kdcompany2018@gmail.com>
2022-09-13 17:51:00 +0300

    fixed moves methods

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -41,7 +41,7 @@ function makeRobot(name, wheels, version) {
   return {
     name,
     wheels,
-    'chip version': version,
+    version,
     coords: {
       x: 0,
       y: 0,
@@ -49,7 +49,7 @@ function makeRobot(name, wheels, version) {
 
     get info() {
       return `name: ${this.name}, `
-        + `chip version: ${this['chip version']}, `
+        + `chip version: ${this.version}, `
         + `wheels: ${this.wheels}`;
     },
 
@@ -58,41 +58,33 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value >= 0) {
+        this.coords.y += value;
       }
-
-      this.coords.y += value;
 
       return this;
     },
 
     goBack(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value >= 0) {
+        this.coords.y -= value;
       }
-
-      this.coords.y -= value;
 
       return this;
     },
 
     goLeft(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value >= 0) {
+        this.coords.x -= value;
       }
-
-      this.coords.x -= value;
 
       return this;
     },
 
     goRight(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value >= 0) {
+        this.coords.x += value;
       }
-
-      this.coords.x += value;
 
       return this;
     },
