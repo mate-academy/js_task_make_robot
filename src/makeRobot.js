a2e1ddabab63edfaf2cce72466c3ef8461ef216b
Anton Vaida <tohan1.ua@gmail.com>
2022-09-13 14:14:49 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,58 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    'name': name,
+    'wheels': wheels,
+    'version': version,
+    'coords': {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, `
+      + `chip version: ${this.version}, `
+      + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    positiveValues(number) {
+      return number > 0 ? number : 0;
+    },
+
+    goForward(Y = 1) {
+      this.coords.y += this.positiveValues(Y);
+
+      return this;
+    },
+    goBack(Y = 1) {
+      this.coords.y -= this.positiveValues(Y);
+
+      return this;
+    },
+    goRight(X = 1) {
+      this.coords.x += this.positiveValues(X);
+
+      return this;
+    },
+    goLeft(X = 1) {
+      this.coords.x -= this.positiveValues(X);
+
+      return this;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
