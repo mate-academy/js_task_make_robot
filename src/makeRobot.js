d45ea4998f4c810f73f7bdce95fdf88a17cbcfbd
fatihsamur <a.fatihsamur@gmail.com>
2022-09-15 20:23:26 +0300

    robot is ready

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,69 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    /* eslint-disable */
+    get info() {
+      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+    },
+    /* eslint-enable */
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(step = 1) {
+      if (this.isValidStep(step)) {
+        this.coords.y += step;
+      }
+
+      return this;
+    },
+
+    goBack(step = 1) {
+      if (this.isValidStep(step)) {
+        this.coords.y -= step;
+      }
+
+      return this;
+    },
+
+    goLeft(step = 1) {
+      if (this.isValidStep(step)) {
+        this.coords.x -= step;
+      }
+
+      return this;
+    },
+
+    goRight(step = 1) {
+      if (this.isValidStep(step)) {
+        this.coords.x += step;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+
+    isValidStep(val) {
+      return Number.isInteger(val) && val > 0;
+    },
+
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
