b549238f941d115f11bf05c3b83ffea947925b77
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-09-15 13:21:48 +0300

    Edit info method

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -47,8 +47,7 @@ function makeRobot(name, wheels, version) {
       y: 0,
     },
     get info() {
-      return `name: ${this.name}, `
-        + `chip version: ${this.version}, `
+      return `name: ${this.name}, chip version: ${this.version}, `
         + `wheels: ${this.wheels}`;
     },
     get location() {
