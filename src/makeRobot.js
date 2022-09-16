fa6b3f905d594e34234d3a01f8f6185f948572c3
Bibicutu <pauleugen@gmail.com>
2022-09-16 19:10:16 +0300

    First commit

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,69 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    get info() {
+      return (`name: ${this.name}, `
+        + `chip version: ${this.version}, `
+        + `wheels: ${this.wheels}`);
+    },
+    get location() {
+      return (
+        `${this.name}: `
+        + `x=${this.coords.x}, `
+        + `y=${this.coords.y}`
+      );
+    },
+    goForward(moves = 1) {
+      if (typeof moves !== 'number' || moves <= 0) {
+        return this;
+      }
+
+      this.coords.y += moves;
+
+      return this;
+    },
+    goBack(moves = 1) {
+      if (typeof moves !== 'number' || moves <= 0) {
+        return this;
+      }
+
+      this.coords.y -= moves;
+
+      return this;
+    },
+    goRight(moves = 1) {
+      if (typeof moves !== 'number' || moves <= 0) {
+        return this;
+      }
+
+      this.coords.x += moves;
+
+      return this;
+    },
+    goLeft(moves = 1) {
+      if (typeof moves !== 'number' || moves <= 0) {
+        return this;
+      }
+
+      this.coords.x -= moves;
+
+      return this;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
