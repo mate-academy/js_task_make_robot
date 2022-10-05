03d710eabbdd80e21512a36135e380ac33f66e77
bagibest <steam.llp@gmail.com>
2022-10-05 09:51:15 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -40,17 +40,21 @@
 
 function makeRobot(name, wheels, version) {
   return {
+    name,
+    wheels,
+    version,
     coords: {
       x: 0,
       y: 0,
     },
 
     get info() {
-      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+      return `name: ${this.name}, chip version: ${this.version},`
+      + ` wheels: ${this.wheels}`;
     },
 
     get location() {
-      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
 
     evacuate() {
