efc0ac2938d7b3e98e44b7f47227ee09d1dd677b
Oleg Lukyanchuk <oleeluk@gmail.com>
2022-10-12 01:25:03 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,80 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+
+    x: 0,
+    y: 0,
+    // get evacuate() ,
+
+    get coords() {
+      return {
+        x: this.x,
+        y: this.y,
+      };
+    },
+    get info() {
+      return `name: ${this.name}, chip version: ${this.version}, `
+      + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.x}, y=${this.y}`;
+    },
+
+    evacuate() {
+      this.x = 1400;
+      this.y = 500;
+
+      return this;
+    },
+
+    goRight(value = 1) {
+      if (value < 0 || typeof value === 'string') {
+        return this;
+      }
+      this.x += value;
+
+      return this;
+    },
+
+    goLeft(value = 1) {
+      if (value < 0 || typeof value === 'string') {
+        return this;
+      }
+      this.x -= value;
+
+      return this;
+    },
+
+    goBack(value = 1) {
+      if (value < 0 || typeof value === 'string') {
+        return this;
+      }
+      this.y -= value;
+
+      return this;
+    },
+
+    goForward(value = 1) {
+      if (value < 0 || typeof value === 'string') {
+        return this;
+      }
+      this.y += value;
+
+      return this;
+    },
+
+  };
+
+  return robot;
 }
+
+// robot.name = name;
+// robot.wheels = wheels;
+// robot.version = version;
 
 module.exports = makeRobot;
