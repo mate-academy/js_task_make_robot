26379a35bbf7bd7ff7bd7892e1db42e00bbd8fd1
Eugene Farafonov <eu.farafonov@gmail.com>
2022-11-04 08:32:42 +0100

    fixed

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,18 +39,18 @@
  */
 function makeRobot(name, wheels, version) {
   const robot = {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
     coords: {
       x: 0,
       y: 0,
     },
 
     get info() {
-      return 'name: ' + this.name
-      + ', chip version: ' + this.version
-      + ', wheels: ' + this.wheels;
+      return `name: ${this.name},`
+      + ` chip version: ${this.version},`
+      + ` wheels: ${this.wheels}`;
     },
 
     get location() {
