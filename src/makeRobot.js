91d289b235ef0554eb0c43399ffb7dea4e473c20
JzSol <111354281+JzSol@users.noreply.github.com>
2022-11-26 12:55:22 +0200

    Finished Task

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,54 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(steps = 1) {
+      this.coords.y += Math.max(0, steps);
+
+      return this;
+    },
+
+    goBack(steps = 1) {
+      this.coords.y -= Math.max(0, steps);
+
+      return this;
+    },
+
+    goRight(steps = 1) {
+      this.coords.x += Math.max(0, steps);
+
+      return this;
+    },
+
+    goLeft(steps = 1) {
+      this.coords.x -= Math.max(0, steps);
+
+      return this;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
