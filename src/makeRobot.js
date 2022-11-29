42d9dab835c4c0219171da6dc473b92246d62425
VladimirEdelmann <VladimirEdelmann@gmail.com>
2022-11-29 11:57:58 +0200

    Add fix

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -49,7 +49,7 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
     },
 
     get location() {
