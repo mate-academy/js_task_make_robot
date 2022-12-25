74b351083734280374912ead04f60421aa5a7cf4
Tamara-Paskalova <forptv@gmail.com>
2022-12-25 19:47:09 +0200

    add logic to makeRobot

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,60 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  return {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0, y: 0,
+    },
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+    goLeft(value) {
+      const number = value || 1;
+
+      if (number > 0) {
+        this.coords.x -= number;
+      }
+
+      return this;
+    },
+    goRight(value) {
+      const number = value || 1;
+
+      if (number > 0) {
+        this.coords.x += number;
+      }
+
+      return this;
+    },
+    goForward(value) {
+      const number = value || 1;
+
+      if (number > 0) {
+        this.coords.y += number;
+      }
+
+      return this;
+    },
+    goBack(value) {
+      const number = value || 1;
+
+      if (number > 0) {
+        this.coords.y -= number;
+      }
+
+      return this;
+    },
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
 }
 
 module.exports = makeRobot;
