9e9b0cc4415ea5aa728e8e6a988ec28a050eba11
Ihor Kuzhel <kuzhelek@gmail.com>
2022-12-28 17:29:35 +0200

    fixed

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -10,7 +10,7 @@ function makeRobot(name, wheels, version) {
       y: 0,
     },
 
-    get info() {
+    get inform() {
       return `name: ${this.name}, `
         + `chip version: ${this.version}, `
         + `wheels: ${this.wheels}`;
