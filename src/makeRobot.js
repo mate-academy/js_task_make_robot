6d7f2e6fcad8511e10825f8cfe215c208c40c83a
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2022-12-30 14:58:01 +0200

    was changed if condition in go-method

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -57,41 +57,33 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y += value;
       }
-
-      this.coords.y += value;
 
       return this;
     },
 
     goBack(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y -= value;
       }
-
-      this.coords.y -= value;
 
       return this;
     },
 
     goLeft(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x -= value;
       }
-
-      this.coords.x -= value;
 
       return this;
     },
 
     goRight(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x += value;
       }
-
-      this.coords.x += value;
 
       return this;
     },
