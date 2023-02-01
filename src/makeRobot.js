801609862f7223afab12505148482aed2421e6ee
Yuliia Tkach <t.yuliia.t@gmail.com>
2023-02-01 23:29:54 +0200

    Make robot solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,80 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(value = 1) {
+      let step = value;
+
+      if (step < 0) {
+        step = 0;
+      }
+
+      this.coords.y += step;
+
+      return this;
+    },
+
+    goBack(value = 1) {
+      let step = value;
+
+      if (step < 0) {
+        step = 0;
+      }
+
+      this.coords.y -= step;
+
+      return this;
+    },
+
+    goRight(value = 1) {
+      let step = value;
+
+      if (step < 0) {
+        step = 0;
+      }
+
+      this.coords.x += step;
+
+      return this;
+    },
+
+    goLeft(value = 1) {
+      let step = value;
+
+      if (step < 0) {
+        step = 0;
+      }
+
+      this.coords.x -= step;
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return this.coords;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
