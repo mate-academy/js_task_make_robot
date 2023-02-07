1460235a72c7d91b44696957797113792078610c
Zavalishyn Oleksandr <zavalishinalexanderdn@gmail.com>
2023-02-08 00:11:55 +0200

    solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,70 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, `
+      + `chip version: ${this.version}, `
+      + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(value = 1) {
+      if (typeof (value) !== 'number' || value < 0) {
+        return this;
+      }
+      value !== 1 ? this.coords.y += value : this.coords.y += 1;
+
+      return this;
+    },
+
+    goBack(value = 1) {
+      if (typeof (value) !== 'number' || value < 0) {
+        return this;
+      }
+      value !== 1 ? this.coords.y -= value : this.coords.y -= 1;
+
+      return this;
+    },
+
+    goRight(value = 1) {
+      if (typeof (value) !== 'number' || value < 0) {
+        return this;
+      }
+      value !== 1 ? this.coords.x += value : this.coords.x += 1;
+
+      return this;
+    },
+
+    goLeft(value = 1) {
+      if (typeof (value) !== 'number' || value < 0) {
+        return this;
+      }
+      value !== 1 ? this.coords.x -= value : this.coords.x -= 1;
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
