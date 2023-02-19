1528331f2a0e0233def81c2d66f24f3ba8a1ae33
siddhadatri <volvik24@gmail.com>
2023-02-19 20:05:40 +0200

    used short syntax

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -40,9 +40,9 @@
 function makeRobot(name, wheels, version) {
   // write code here
   const robot = {
-    name: name,
+    name,
     chip: version,
-    wheels: wheels,
+    wheels,
     coords: {
       x: 0,
       y: 0,
