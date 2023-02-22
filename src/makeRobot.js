a043b030c0ebe6a1c7bca315a65a89f8daaf3e48
Maindloll <valeriy.mikheyev@gmail.com>
2023-02-22 15:40:44 +0200

    makeRobot

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,6 +39,63 @@
  */
 function makeRobot(name, wheels, version) {
   // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    goForward(distance = 1) {
+      if (distance < 0) {
+        return this;
+      }
+      this.coords.y += distance;
+
+      return this;
+    },
+    goBack(distance = 1) {
+      if (distance < 0) {
+        return this;
+      }
+      this.coords.y -= distance;
+
+      return this;
+    },
+    goRight(distance = 1) {
+      if (distance < 0) {
+        return this;
+      }
+      this.coords.x += distance;
+
+      return this;
+    },
+    goLeft(distance = 1) {
+      if (distance < 0) {
+        return this;
+      }
+      this.coords.x -= distance;
+
+      return this;
+    },
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+    get info() {
+      // eslint-disable-next-line max-len
+      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
