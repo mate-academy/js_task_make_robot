05d6548aea85a8addc32eea04c025c03f989a6a9
Ivan Kalyenichenko <ikalenicenko205@gmail.com>
2023-03-02 19:21:28 +0200

    Rework

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,6 +39,10 @@
  */
 function makeRobot(name, wheels, version) {
   const robot = {
+    name,
+    wheels,
+    version,
+
     coords: {
       x: 0,
       y: 0,
@@ -82,11 +86,12 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+      return `name: ${this.name}`
+      + `, chip version: ${this.version}, wheels: ${this.wheels}`;
     },
 
     get location() {
-      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
   };
 
