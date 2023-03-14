6aa4a264d0ae8edf23e76154200cad9a56fccbaa
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-14 18:21:48 +0200

    robot is ready for rock-n-roll

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,63 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      // eslint-disable-next-line max-len
+      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(distance = 1) {
+      if (distance >= 0) {
+        this.coords.y += distance;
+      }
+
+      return robot;
+    },
+
+    goBack(distance = 1) {
+      if (distance >= 0) {
+        this.coords.y -= distance;
+      }
+
+      return robot;
+    },
+
+    goRight(distance = 1) {
+      if (distance >= 0) {
+        this.coords.x += distance;
+      }
+
+      return robot;
+    },
+
+    goLeft(distance = 1) {
+      if (distance >= 0) {
+        this.coords.x -= distance;
+      }
+
+      return robot;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
