5856402a94fd4a49e49734c736488ed59aa0e09e
Amadeusz Michalowski <amadeusz.michalowski@gmail.com>
2023-04-04 01:26:25 +0200

    changes

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,9 +39,9 @@
  */
 function makeRobot(name, wheels, version) {
   const robot = {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
     coords: {
       x: 0,
       y: 0,
