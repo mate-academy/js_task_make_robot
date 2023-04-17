adc7d397babbc343549262625fa26a18717062da
Oleksandr Voronov <alexdroid33@gmail.com>
2023-04-17 13:35:57 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -11,7 +11,7 @@
  * The robot coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
  *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
- *  - Provide the coordsinates of your location via getter location.
+ *  - Provide the coordinates of your location via getter location.
  *    robot.location === '%name%: x=14, y=21'
  *  - Have methods to move goForward, goBack, goRight, goLeft.
  *  - Movement methods must be able to be used with a chain.
@@ -19,11 +19,11 @@
  *  - Default methods that move the work by 1 in the right direction.
  *    This value can be increased by passing the desired number to the method.
  *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordsinates of the robot must be stored in the object coordss,
+ *  - The coordinates of the robot must be stored in the object coords,
  *    the keys x and y inside the robot.
  *  - The robot must be able to request the evacuation of robot.evacuate(),
  *    which will call rescuers and transfer it to the service center
- *    at the coordsinates x: 1400, y: 500.
+ *    at the coordinates x: 1400, y: 500.
  *
  * @typedef {object} Robot
  * @property {string} name
@@ -61,7 +61,7 @@ function makeRobot(name, wheels, version) {
         this.coords.y += step;
       };
 
-      return robot;
+      return this;
     },
 
     goBack(step = 1) {
@@ -69,7 +69,7 @@ function makeRobot(name, wheels, version) {
         this.coords.y -= step;
       };
 
-      return robot;
+      return this;
     },
 
     goRight(step = 1) {
@@ -77,7 +77,7 @@ function makeRobot(name, wheels, version) {
         this.coords.x += step;
       };
 
-      return robot;
+      return this;
     },
 
     goLeft(step = 1) {
@@ -85,7 +85,7 @@ function makeRobot(name, wheels, version) {
         this.coords.x -= step;
       };
 
-      return robot;
+      return this;
     },
 
     evacuate() {
