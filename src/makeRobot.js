6ac153319cea5a4f2164b466dac5e6d1631f815b
Andrzej Pietkiewicz <pietan@me.com>
2023-04-24 13:08:03 +0200

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,71 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return ('name: ' + this.name + ', chip version: '
+      + this.version + ', wheels: ' + this.wheels);
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    // set steps(move) {
+    //   const moves = move.split(', ');
+
+    //   for (const key in moves) {
+    //     return move[key];
+    //   }
+    // },
+
+    goRight(x = 1) {
+      if (x < 0) { } else {
+        this.coords.x += x;
+      }
+
+      return robot;
+    },
+
+    goLeft(x = 1) {
+      if (x < 0) { } else {
+        this.coords.x -= x;
+      }
+
+      return robot;
+    },
+
+    goBack(y = 1) {
+      if (y < 0) { } else {
+        this.coords.y -= y;
+      }
+
+      return robot;
+    },
+
+    goForward(y = 1) {
+      if (y < 0) { } else {
+        this.coords.y += y;
+      }
+
+      return robot;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
