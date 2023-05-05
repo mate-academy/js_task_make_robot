3a37e91b4a74e57b6e27cf3cea3bc6ab0c810729
Maksym Nemera <nickmitchell@ukr.net>
2023-05-05 11:40:47 +0300

    add task solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,78 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return this.coords;
+    },
+
+    goLeft(value = 1) {
+      if (value < 0) {
+        this.coords.x -= 0;
+      }
+
+      if (value > 0) {
+        this.coords.x -= value;
+      }
+
+      return this;
+    },
+
+    goRight(value = 1) {
+      if (value < 0) {
+        this.coords.x += 0;
+      }
+
+      if (value > 0) {
+        this.coords.x += value;
+      }
+
+      return this;
+    },
+
+    goBack(value = 1) {
+      if (value < 0) {
+        this.coords.y -= 0;
+      }
+
+      if (value > 0) {
+        this.coords.y -= value;
+      }
+
+      return this;
+    },
+
+    goForward(value = 1) {
+      if (value < 0) {
+        this.coords.y += 0;
+      }
+
+      if (value > 0) {
+        this.coords.y += value;
+      }
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
