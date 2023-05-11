0923bdcb818382e8bc60f05aca3fed9b8c489d76
Viktor Panchenko <panch.v1811@gmail.com>
2023-05-11 14:36:03 +0200

    suggested changes

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -49,45 +49,50 @@ function makeRobot(name, wheels, version) {
   };
 
   const robot = {
-    get info() {
-      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
-    },
+
+    name,
+    wheels,
+    version,
 
     coords: {
       x: startCoords.x,
       y: startCoords.y,
     },
 
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+
     get location() {
       return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
 
-    goForward(value = 1) {
-      if (value > 0) {
-        this.coords.y += value;
+    goForward(step = 1) {
+      if (step > 0) {
+        this.coords.y += step;
       }
 
       return this;
     },
 
-    goBack(value = 1) {
-      if (value > 0) {
-        this.coords.y -= value;
+    goBack(step = 1) {
+      if (step > 0) {
+        this.coords.y -= step;
       }
 
       return this;
     },
-    goRight(value = 1) {
-      if (value > 0) {
-        this.coords.x += value;
+    goRight(step = 1) {
+      if (step > 0) {
+        this.coords.x += step;
       }
 
       return this;
     },
 
-    goLeft(value = 1) {
-      if (value > 0) {
-        this.coords.x -= value;
+    goLeft(step = 1) {
+      if (step > 0) {
+        this.coords.x -= step;
       }
 
       return this;
