7b1e1861c4e8661175c19da41a12d2ebbc9265af
Vania Yarovyi <yarovyi@gmail.com>
2023-05-11 11:26:52 +0300

    add solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,68 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    get info() {
+      return `name: ${robot.name}, chip version: ${robot.version}, wheels: ${
+        robot.wheels}`;
+    },
+    get location() {
+      return `${robot.name}: x=${robot.coords.x}, y=${robot.coords.y}`;
+    },
+    goForward(step = 1) {
+      let numbersOfSteps = step;
+
+      if (numbersOfSteps < 0) {
+        numbersOfSteps = 0;
+      }
+      robot.coords.y += numbersOfSteps;
+
+      return robot;
+    },
+    goBack(step = 1) {
+      let numbersOfSteps = step;
+
+      if (numbersOfSteps < 0) {
+        numbersOfSteps = 0;
+      }
+      robot.coords.y -= numbersOfSteps;
+
+      return robot;
+    },
+    goRight(step = 1) {
+      let numbersOfSteps = step;
+
+      if (numbersOfSteps < 0) {
+        numbersOfSteps = 0;
+      }
+      robot.coords.x += numbersOfSteps;
+
+      return robot;
+    },
+    goLeft(step = 1) {
+      let numbersOfSteps = step;
+
+      if (numbersOfSteps < 0) {
+        numbersOfSteps = 0;
+      }
+      robot.coords.x -= numbersOfSteps;
+
+      return robot;
+    },
+    evacuate() {
+      robot.coords.x = 1400;
+      robot.coords.y = 500;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
