c980f7a0edb99accee9259b28e0a4f8cb4d33d94
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-05-12 13:22:14 +0300

    fixes

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -6,7 +6,7 @@
  * with the team! Are you in business As a test task, you will need to
  * program our equipment that makes robots.
  *
- * Create a makeRobot function that takes the string name and the number
+ * Create a makeRobot function that takes the string name and the step
  * wheels, version and returns the robot object.
  * The robot coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
@@ -17,8 +17,8 @@
  *  - Movement methods must be able to be used with a chain.
  *    robot.goForward().goForward().goForward().goLeft()
  *  - Default methods that move the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
+ *    This value can be increased by passing the desired step to the method.
+ *    Negative steps should not affect the location of the robot. goLeft(3)
  *  - The coordinates of the robot must be stored in the object coords,
  *    the keys x and y inside the robot.
  *  - The robot must be able to request the evacuation of robot.evacuate(),
@@ -41,7 +41,7 @@ function makeRobot(name, wheels, version) {
   const robot = {
     name,
     wheels,
-    'chip version': [version],
+    version,
     coords: {
       x: 0,
       y: 0,
@@ -49,45 +49,45 @@ function makeRobot(name, wheels, version) {
 
     get info() {
       return `name: ${this.name}, chip version: \
-${this['chip version']}, wheels: ${this.wheels}`;
+${this.version}, wheels: ${this.wheels}`;
     },
 
     get location() {
       return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
 
-    goForward(number = 1) {
-      if (number <= 0) {
+    goForward(step = 1) {
+      if (step <= 0) {
         return this;
       }
-      this.coords.y += number;
+      this.coords.y += step;
 
       return this;
     },
 
-    goBack(number = 1) {
-      if (number <= 0) {
+    goBack(step = 1) {
+      if (step <= 0) {
         return this;
       }
-      this.coords.y -= number;
+      this.coords.y -= step;
 
       return this;
     },
 
-    goRight(number = 1) {
-      if (number <= 0) {
+    goRight(step = 1) {
+      if (step <= 0) {
         return this;
       }
-      this.coords.x += number;
+      this.coords.x += step;
 
       return this;
     },
 
-    goLeft(number = 1) {
-      if (number <= 0) {
+    goLeft(step = 1) {
+      if (step <= 0) {
         return this;
       }
-      this.coords.x -= number;
+      this.coords.x -= step;
 
       return this;
     },
