8be9f9eb9772c8d33910d845873256d50a2e9320
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-06-14 13:20:47 +0300

    Minor change to avoid disabling eslint

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -53,8 +53,8 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      // eslint-disable-next-line max-len
-      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+      return `name: ${this.name}, chip version: ${this.version}, `
+        + `wheels: ${this.wheels}`;
     },
 
     goBack(step = 1) {
