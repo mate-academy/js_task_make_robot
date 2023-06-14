e7b1ebeba3d2b396d9d7864ba2487d46687f5419
Serhii Melnyk <serzh2001melnyk@gmail.com>
2023-06-14 10:26:59 +0300

    Fix mistakes

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -80,8 +80,8 @@ function makeRobot(name, wheels, version) {
     },
 
     get info() {
-      // eslint-disable-next-line max-len
-      return `name: ${this.name}, chip version: ${this.version}, wheels: ${this.wheels}`;
+      return `name: ${this.name}, chip version: ${this.version}, `
+        + `wheels: ${this.wheels}`;
     },
 
     get location() {
