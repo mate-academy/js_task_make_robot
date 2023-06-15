dcc0280015b2a64ce5f49d9cc2175f1f10609da0
Artem Andrushchenko <artem.abdrushenko213654@gmail.com>
2023-06-15 08:47:51 +0300

    add task solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,57 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0, y: 0,
+    },
+    goForward(distance = 1, step = 1) {
+      if (distance > 0) {
+        this.coords.y += Math.abs(distance) * step;
+      }
+
+      return this;
+    },
+    goBack(distance = 1, step = 1) {
+      if (distance > 0) {
+        this.coords.y -= Math.abs(distance) * step;
+      }
+
+      return this;
+    },
+    goLeft(distance = 1, step = 1) {
+      if (distance > 0) {
+        this.coords.x -= Math.abs(distance) * step;
+      }
+
+      return this;
+    },
+    goRight(distance = 1, step = 1) {
+      if (distance > 0) {
+        this.coords.x += Math.abs(distance) * step;
+      }
+
+      return this;
+    },
+    get info() {
+      return `name: ${name}, chip version: ${version}, wheels: ${wheels}`;
+    },
+    get location() {
+      return `${name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+    evacuate() {
+      this.coords = {
+        x: 1400, y: 500,
+      };
+
+      return this;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
