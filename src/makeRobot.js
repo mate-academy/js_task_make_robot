bbf946d86ca70d3d67422444c415c32ff149b6cd
Faridun Mainaev <farid.mainaiev@gmail.com>
2023-06-27 14:12:06 +0300

    second commit

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -37,63 +37,6 @@
  *
  * @return {Robot}
  */
-// function makeRobot(name, wheels, version) {
-//   const robot = {
-//     name,
-//     wheels,
-//     version,
-//     coords: {
-//       x: 0, y: 0,
-//     },
-
-//     get info() {
-//       return `
-//         name: ${this.name},
-//         chip version: ${this.version},
-//         wheels: ${this.wheels}`;
-//     },
-
-//     get location() {
-//       return `
-//         ${this.name}:
-//         x=${this.coords.x},
-//         y=${this.coords.y}`;
-//     },
-
-//     goForward(distance = 1) {
-//       this.coords.y += distance;
-
-//       return this;
-//     },
-
-//     goBack(distance = 1) {
-//       this.coords.y -= distance;
-
-//       return this;
-//     },
-
-//     goRight(distance = 1) {
-//       this.coords.x += distance;
-
-//       return this;
-//     },
-
-//     goLeft(distance = 1) {
-//       this.coords.x -= distance;
-
-//       return this;
-//     },
-
-//     evacuate() {
-//       this.coords.x = 1400;
-//       this.coords.y = 500;
-
-//       return this;
-//     },
-//   };
-
-//   return robot;
-// }
 
 function makeRobot(name, wheels, version) {
   const robot = {
