a7574ab42c803ef722a3f6cba20d94ed1c6a3c10
Andrii Solovei <solovey98.info@gmail.com>
2023-07-11 23:27:50 +0300

    fixed code, now method return this only once

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,9 +39,9 @@
  */
 function makeRobot(name, wheels, version) {
   const robot = {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
     coords: {
       x: 0,
       y: 0,
@@ -56,34 +56,30 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y += value;
       }
-      this.coords.y += value;
 
       return this;
     },
     goBack(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y -= value;
       }
-      this.coords.y -= value;
 
       return this;
     },
     goLeft(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x -= value;
       }
-      this.coords.x -= value;
 
       return this;
     },
     goRight(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x += value;
       }
-      this.coords.x += value;
 
       return this;
     },
