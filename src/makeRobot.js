377fa4e940579666b207201ff27c0f9cfdd87c6d
arturwisnia <wisnia3008@interia.pl>
2023-07-26 21:14:58 +0200

    add task solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,49 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const robot = {
+    name: name,
+    wheels: wheels,
+    version: version,
+    coords: {
+      x: 0, y: 0,
+    }, // Początkowe współrzędne (0, 0)
+    goForward: function(steps = 1) {
+      this.coords.y += steps;
+
+      return this;
+    },
+    goBack: function(steps = 1) {
+      this.coords.y -= steps;
+
+      return this;
+    },
+    goRight: function(steps = 1) {
+      this.coords.x += steps;
+
+      return this;
+    },
+    goLeft: function(steps = 1) {
+      this.coords.x -= steps;
+
+      return this;
+    },
+    evacuate: function() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+
+      return this;
+    },
+    get info() {
+      return `name: ${this.name}, chip version:`
+        + ` ${this.version}, wheels: ${this.wheels}`;
+    },
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
