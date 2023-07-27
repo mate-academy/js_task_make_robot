250f1c262ee696674450f3c5d139522e3617c89f
Patryk Filipiak <patryk.fy@o2.pl>
2023-07-27 10:13:02 +0200

    used ifs except tenary and object name is lowercase :)

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -6,7 +6,7 @@
  * with the team! Are you in business As a test task, you will need to
  * program our equipment that makes robots.
  *
- * Create a makeRobot function that takes the string name and the number
+ * Create a makeRobot function that takes the string name and the parseInt
  * wheels, version and returns the robot object.
  * The robot coming off the assembly line must be able to:
  *  - Provide information about yourself through getter info.
@@ -17,8 +17,8 @@
  *  - Movement methods must be able to be used with a chain.
  *    robot.goForward().goForward().goForward().goLeft()
  *  - Default methods that move the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
+ *    This value can be increased by passing the desired parseInt to the method.
+ *    Negative parseInts should not affect the location of the robot. goLeft(3)
  *  - The coordinates of the robot must be stored in the object coords,
  *    the keys x and y inside the robot.
  *  - The robot must be able to request the evacuation of robot.evacuate(),
@@ -27,13 +27,13 @@
  *
  * @typedef {object} Robot
  * @property {string} name
- * @property {number} wheels
- * @property {number} version
+ * @property {parseInt} wheels
+ * @property {parseInt} version
  * @property {function} info
  *
  * @param {string} name
- * @param {number} wheels
- * @param {number} version
+ * @param {parseInt} wheels
+ * @param {parseInt} version
  *
  * @return {Robot}
  */
@@ -41,7 +41,7 @@ const EVACUATE_POSITION_X = 1400;
 const EVACUATE_POSITION_Y = 500;
 
 function makeRobot(name, wheels, version) {
-  const Robot = {
+  const robot = {
     name,
     wheels,
     version,
@@ -60,25 +60,33 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(steps = 1) {
-      this.coords.y += Number(steps) > 0 ? steps : 0;
+      if (steps > 0) {
+        this.coords.y += steps;
+      }
 
       return this;
     },
 
     goBack(steps = 1) {
-      this.coords.y -= Number(steps) > 0 ? steps : 0;
+      if (steps > 0) {
+        this.coords.y -= steps;
+      }
 
       return this;
     },
 
     goLeft(steps = 1) {
-      this.coords.x -= Number(steps) > 0 ? steps : 0;
+      if (steps > 0) {
+        this.coords.x -= steps;
+      }
 
       return this;
     },
 
     goRight(steps = 1) {
-      this.coords.x += Number(steps) > 0 ? steps : 0;
+      if (steps > 0) {
+        this.coords.x += steps;
+      }
 
       return this;
     },
@@ -90,7 +98,7 @@ function makeRobot(name, wheels, version) {
 
   };
 
-  return Robot;
+  return robot;
 }
 
 module.exports = makeRobot;
