ce7c9164f4da0b0573c59328c07307a4450713b1
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-08-02 20:49:33 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * Mate Robot Factory impressed by your success, they are ready to accept
+ * Mate Robot Factory is impressed with your success, they are ready to accept
  * you into the Tech team, you will learn to program robots together
- * with the team! Are you in business As a test task, you will need to
+ * with the team! Are you in business? As a test task, you will need to
  * program our equipment that makes robots.
  *
  * Create a makeRobot function that takes the string name and the number
@@ -38,7 +38,67 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const EVACUATION_COORDS_X = 1400;
+  const EVACUATION_COORDS_Y = 500;
+
+  const robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, `
+      + `chip version: ${this.version}, `
+      + `wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(move = 1) {
+      if (move > 0) {
+        this.coords.y += move;
+      }
+
+      return this;
+    },
+
+    goBack(move = 1) {
+      if (move > 0) {
+        this.coords.y -= move;
+      }
+
+      return this;
+    },
+
+    goRight(move = 1) {
+      if (move > 0) {
+        this.coords.x += move;
+      }
+
+      return this;
+    },
+
+    goLeft(move = 1) {
+      if (move > 0) {
+        this.coords.x -= move;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = EVACUATION_COORDS_X;
+      this.coords.y = EVACUATION_COORDS_Y;
+    },
+  };
+
+  return robot;
 }
 
 module.exports = makeRobot;
