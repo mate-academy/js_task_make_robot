fde3e07c40e871d84a5d7f9944f36b56ced7ffab
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-08-02 12:05:21 +0300

    add task solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -38,7 +38,79 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const Robot = {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+
+    get info() {
+      return `name: ${this.name}, chip version: ${this.version},`
+        + ` wheels: ${this.wheels}`;
+    },
+
+    get location() {
+      return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
+    },
+
+    goForward(value) {
+      if (value) {
+        if (value > 0) {
+          this.coords.y += value;
+        }
+      } else {
+        this.coords.y++;
+      }
+
+      return this;
+    },
+
+    goBack(value) {
+      if (value) {
+        if (value > 0) {
+          this.coords.y -= value;
+        }
+      } else {
+        this.coords.y--;
+      }
+
+      return this;
+    },
+
+    goLeft(value) {
+      if (value) {
+        if (value > 0) {
+          this.coords.x -= value;
+        }
+      } else {
+        this.coords.x--;
+      }
+
+      return this;
+    },
+
+    goRight(value) {
+      if (value) {
+        if (value > 0) {
+          this.coords.x += value;
+        }
+      } else {
+        this.coords.x++;
+      }
+
+      return this;
+    },
+
+    evacuate() {
+      this.coords.x = 1400;
+      this.coords.y = 500;
+    },
+  };
+
+  return Robot;
 }
 
 module.exports = makeRobot;
