ec32fde18cbe002f701dd01b1bb29d975f90be44
Dmytro Hrynchyi <desevoker@gmail.com>
2023-08-04 13:37:25 +0300

    Solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -1,30 +1,6 @@
 'use strict';
 
 /**
- * Mate Robot Factory impressed by your success, they are ready to accept
- * you into the Tech team, you will learn to program robots together
- * with the team! Are you in business As a test task, you will need to
- * program our equipment that makes robots.
- *
- * Create a makeRobot function that takes the string name and the number
- * wheels, version and returns the robot object.
- * The robot coming off the assembly line must be able to:
- *  - Provide information about yourself through getter info.
- *    robot.info === 'name:%name%, chip version: %version%, wheels: %wheels%'
- *  - Provide the coordinates of your location via getter location.
- *    robot.location === '%name%: x=14, y=21'
- *  - Have methods to move goForward, goBack, goRight, goLeft.
- *  - Movement methods must be able to be used with a chain.
- *    robot.goForward().goForward().goForward().goLeft()
- *  - Default methods that move the work by 1 in the right direction.
- *    This value can be increased by passing the desired number to the method.
- *    Negative numbers should not affect the location of the robot. goLeft(3)
- *  - The coordinates of the robot must be stored in the object coords,
- *    the keys x and y inside the robot.
- *  - The robot must be able to request the evacuation of robot.evacuate(),
- *    which will call rescuers and transfer it to the service center
- *    at the coordinates x: 1400, y: 500.
- *
  * @typedef {object} Robot
  * @property {string} name
  * @property {number} wheels
@@ -38,7 +14,65 @@
  * @return {Robot}
  */
 function makeRobot(name, wheels, version) {
-  // write code here
+  const DEFAULT_STEPS = 1;
+  const START_COORD_X = 0;
+  const START_COORD_Y = 0;
+  const SERVICE_COORD_X = 1400;
+  const SERVICE_COORD_Y = 500;
+
+  return {
+    name,
+    wheels,
+    version,
+    coords: {
+      x: START_COORD_X,
+      y: START_COORD_Y,
+    },
+
+    goForward(steps = DEFAULT_STEPS) {
+      return this.move(0, this.normalizeSteps(steps));
+    },
+
+    goBack(steps = DEFAULT_STEPS) {
+      return this.move(0, -this.normalizeSteps(steps));
+    },
+
+    goRight(steps = DEFAULT_STEPS) {
+      return this.move(this.normalizeSteps(steps), 0);
+    },
+
+    goLeft(steps = DEFAULT_STEPS) {
+      return this.move(-this.normalizeSteps(steps), 0);
+    },
+
+    evacuate() {
+      this.coords.x = SERVICE_COORD_X;
+      this.coords.y = SERVICE_COORD_Y;
+    },
+
+    move(x = 0, y = 0) {
+      this.coords.x += x;
+      this.coords.y += y;
+
+      return this;
+    },
+
+    normalizeSteps(steps) {
+      return steps > 0 ? steps : 0;
+    },
+
+    get location() {
+      return `${this.name}: `
+        + `x=${this.coords.x}, `
+        + `y=${this.coords.y}`;
+    },
+
+    get info() {
+      return `name: ${this.name}, `
+        + `chip version: ${this.version}, `
+        + `wheels: ${this.wheels}`;
+    },
+  };
 }
 
 module.exports = makeRobot;
