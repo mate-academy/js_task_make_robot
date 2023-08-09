6c0b13e56a4dcaeaeca578e412de43557f4c5092
Maria Snegireva <mariyasn88@gmail.com>
2023-08-09 18:10:01 +0300

    add task solution

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -56,37 +56,33 @@ function makeRobot(name, wheels, version) {
     },
 
     goForward(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y += 1 * value;
       }
-      this.coords.y += 1 * value;
 
       return this;
     },
 
     goBack(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.y -= 1 * value;
       }
-      this.coords.y -= 1 * value;
 
       return this;
     },
 
     goRight(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x += 1 * value;
       }
-      this.coords.x += 1 * value;
 
       return this;
     },
 
     goLeft(value = 1) {
-      if (value < 0) {
-        return this;
+      if (value > 0) {
+        this.coords.x -= 1 * value;
       }
-      this.coords.x -= 1 * value;
 
       return this;
     },
