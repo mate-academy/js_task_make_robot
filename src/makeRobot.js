a011b3836134c49e393692d692f4e5b7dce1e801
Bahtijar <no4kar@gmail.com>
2023-08-09 12:33:18 +0200

    correctio-1

--- src/makeRobot.js
+++ src/makeRobot.js
@@ -39,20 +39,24 @@
  */
 function makeRobot(name, wheels, version) {
   return {
-    name: name,
-    wheels: wheels,
-    version: version,
+    name,
+    wheels,
+    version,
+
     coords: {
       x: 0,
       y: 0,
     },
+
     get info() {
       return `name: ${this.name}`
       + `, chip version: ${this.version}, wheels: ${this.wheels}`;
     },
+
     get location() {
       return `${this.name}: x=${this.coords.x}, y=${this.coords.y}`;
     },
+
     goForward(step = 1) {
       if (step > 0) {
         this.coords.y += step;
@@ -60,6 +64,7 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
+
     goBack(step = 1) {
       if (step > 0) {
         this.coords.y -= step;
@@ -67,6 +72,7 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
+
     goRight(step = 1) {
       if (step > 0) {
         this.coords.x += step;
@@ -74,6 +80,7 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
+
     goLeft(step = 1) {
       if (step > 0) {
         this.coords.x -= step;
@@ -81,9 +88,14 @@ function makeRobot(name, wheels, version) {
 
       return this;
     },
-    evacuate(serviceCenterX = 1400, serviceCenterY = 500) {
-      this.coords.x = serviceCenterX;
-      this.coords.y = serviceCenterY;
+
+    evacuate() {
+      const serviceCenter = {
+        x: 1400, y: 500,
+      };
+
+      this.coords.x = serviceCenter.x;
+      this.coords.y = serviceCenter.y;
     },
   };
 }
